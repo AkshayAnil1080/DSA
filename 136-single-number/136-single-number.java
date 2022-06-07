ba39e3978afde77bd37431dcb4db25f5c6837fd6
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-06-07 13:43:25 +0530

    Time: 1 ms (99.30%), Space: 50.9 MB (32.54%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,15 +1,13 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        
-        HashMap<Integer, Integer> m = new HashMap<>();
-        for( int i =0;i<nums.length; i++)
+        int ans =nums[0];
+        for( int i=1;i<nums.length;i++)
         {
-            m.put(nums[i], m.getOrDefault(nums[i],0)+1);                                                if((m.get(nums[i]))==2)
-                    m.remove(nums[i]);
+            ans ^=nums[i];
         }
-    for( Map.Entry<Integer, Integer> e :  m.entrySet())
-        return e.getKey(); 
-       
-        return 0;
+        return ans;
     }
 }
+// Idea: a^a=0
+//     TC:O(n)
+//     SC:O(1)
