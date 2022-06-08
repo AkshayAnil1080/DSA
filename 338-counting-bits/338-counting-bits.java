8a66540a62d03f7da9aa750437e25ca6d0681c92
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-06-09 01:12:34 +0530

    Time: 2 ms (79.57%), Space: 48.2 MB (42.63%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,24 +1,16 @@
 class Solution {
     public int[] countBits(int num) {
         
-       
+//         jsut reatin the last bit set or not, remaining ie n/2 can be found from same table theta(n) solution
         int ans[] = new int[num+1];
-        for( int i =0; i<=num;i++)
+        ans[0]=0;
+        for( int i =1; i<=num;i++)
         {
-            ans[i]=count_set_bits(i);
+            ans[i]= (i&1) + ans[i/2];
         }
         return ans;
     }
-//     theta(setbitcount) // brain kerninngams algo
-    int count_set_bits(int n)
-    {int c=0;
-         while(n>0)
-         {
-             n=n&(n-1);
-                 c++;
-         }
-     return c;
-    }
+
     
 }
 // TC: O(nlogn)
