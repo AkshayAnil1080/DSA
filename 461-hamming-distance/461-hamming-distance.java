436f6425b6d6e4a36db435b55ec97ef7866f26f4
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-06-09 01:32:14 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (29.75%) - LeetHub

--- 461-hamming-distance/461-hamming-distance.java
+++ 461-hamming-distance/461-hamming-distance.java
@@ -1,31 +1,19 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int a[] = new int [256];
-        int b[] = new int[256];
-        
-        int ind=255;
-        while(x>0)
-        {
-            if((x&1)==1)
-                a[ind]=1;
-            ind--;
-            x=x/2; // or x>>=2;
-        }
-         ind=255;
-        while(y>0)
-        {
-            if((y&1)==1)
-                b[ind]=1;
-            ind--;
-            y=y/2; // or y>>=2;
-        }
-        
+      
+        int till_loop = x>y? x:y;
         int ans=0;
-        for( int i =0;i<256;i++)
+        while(till_loop>0)
         {
-            if(a[i]!=b[i])
+            if((y&1) != (x&1))
                 ans++;
+          
+            y=y/2; // or y>>=2;
+            x=x/2;
+            till_loop/=2;
         }
+        
+        
         return ans;
     }
 }
