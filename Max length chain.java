b67cbc122131f76d60b473ee522181daa6f99f03
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-08-13 23:52:22 +0530

    Create Max length chain.java

---
+++ Max length chain.java
@@ -0,0 +1,29 @@
+https://practice.geeksforgeeks.org/problems/max-length-chain/1
+
+
+
+
+class GfG
+{
+    int maxChainLength(Pair arr[], int n)
+    {
+       // your code here
+       
+       Arrays.sort(arr, (a,b) -> a.y-b.y);
+       int b=arr[0].y;
+       int ans=1; // 1 ele will itselg be the anwer if only first ele was present.
+       
+       for( int i =1;i<n ;i++)
+       {
+           int c =arr[i].x;
+           if(c>b)
+           {
+               b=arr[i].y;  // update b with curr y.
+               ans++;
+           }
+       }
+       return ans;
+    }
+}
+
+//O(nlogn) , O(1)
