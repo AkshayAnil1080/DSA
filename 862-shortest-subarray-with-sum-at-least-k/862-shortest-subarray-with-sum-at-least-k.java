90d3f2d074925dbd51852a2e41e648fc4f0b3cc6
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-08-26 13:09:44 +0530

    Time: 70 ms (46.03%), Space: 123.6 MB (34.60%) - LeetHub

--- 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.java
+++ 862-shortest-subarray-with-sum-at-least-k/862-shortest-subarray-with-sum-at-least-k.java
@@ -1,34 +1,57 @@
-class Solution {
-    public int shortestSubarray(int[] nums, int k) {
-        int n=nums.length;
-        long pre[] = new long[n+1];
-        for( int i=0 ; i<n; i++)
-        {
-            pre[i+1] = pre[i]+nums[i];
-        }
-        
-        int ans = Integer.MAX_VALUE;
-        LinkedList<Integer> q = new LinkedList<>();
-        
-        // we need the cumm_sum at every i, so made pre array just for convenice and aadding directly the index in LL(just removing last, then again - and removing first) O(1)
-        for( int i =0; i<=n; i++)
-        {
-            //maintng monotonous increasingos sum values and popping the last until we get ele < sum
-            
-                while(!q.isEmpty() && pre[i] <= pre[q.peekLast()])
-                {
-                    q.removeLast();
-                }
-                // //reduce the window  size from left and keep on decereasing unti cummu_Sum <targ and return the window length;
-                while(!q.isEmpty() && pre[i] - pre[q.peekFirst()]>=k)
-                {
-                    ans = Math.min(ans, i-q.removeFirst());
-                }
-                
-                q.add(i);
-            
-        }
-        return ans==Integer.MAX_VALUE ? -1 : ans;
-        
+// TC: O(2n) - each node is visited at most twice 
+//SC: O(n) for pair
+class Pair
+{
+    Long x; int y;
+    Pair(Long x, int y)
+    {
+        this.x=x;
+        this.y=y;
     }
 }
+class Solution {
+    public int shortestSubarray(int[] nums, int k) {
+           
+       ArrayDeque<Pair> dq = new ArrayDeque<>();
+        long ans = Integer.MAX_VALUE;
+        int n = nums.length;     
+        long sum=0;
+        for( int i =0; i<n; i++)
+        {
+            sum += nums[i];
+        
+            if(sum>=k) ans=Math.min(ans,i+1); //1. obv
+            
+            
+            //reduce the window  size from left and keep on decereasing unti cummu_Sum <12 and return the window length; line 33
+            Pair curr = new Pair((long)Integer.MIN_VALUE, Integer.MIN_VALUE);
+            while(!dq.isEmpty() && sum-dq.getFirst().x >=k)
+            {
+                curr=  dq.getFirst();
+                dq.pollFirst();
+            }
+            
+            if(curr.x!=Integer.MIN_VALUE)
+            {
+                ans = Math.min(i-curr.y, ans);
+            }
+                
+            // we have to maintain moto increasing order, so if sum obtained  > last ele in dequeu, keep popping out pop last until we get sum < Pair.x
+              while(!dq.isEmpty() && sum<=dq.getLast().x)
+            {
+                dq.pollLast();
+            }
+            dq.addLast(new Pair(sum,i));
+            
+            
+        }
+        return ans==Integer.MAX_VALUE? -1:(int)ans;
+    }
+}
+
+//Sim, to 209
+// 209. Minimum Size Subarray Sum>=k
+//1. keep goinf right and summing
+//2. as asson as sum >=target => store the window size, subtract the left most ele, store the size again, 
+//and move left one ptr ahead and keep doing it
+// when s2 while loop over => again move forward for next ele.
