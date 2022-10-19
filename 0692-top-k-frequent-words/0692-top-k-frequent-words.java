2605e86bb444437a7aece1b08566d5b3334999a0
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-10-19 15:31:59 +0530

    Time: 6 ms (96.26%), Space: 42.3 MB (97.62%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> ans = new ArrayList<>();
+            int n = words.length;
+         
+        HashMap<String,Integer> map = new HashMap<>();
+         for(int i =0; i<n; i++)
+         {
+             map.put(words[i], map.getOrDefault(words[i],0)+1);
+         }
+        
+    
+        PriorityQueue<String> pq = new PriorityQueue<>((a,b) -> map.get(a).equals(map.get(b)) ? b.compareTo(a) : map.get(a)-map.get(b)); // sorting the string in min heap and if string exists with same count then sort it via freq
+       for(String curr : map.keySet())
+       {
+           
+           pq.add(curr);
+           if(pq.size()>k)
+               pq.remove();
+       }
+        for(int i=0; i<k; i++)
+        { 
+            ans.add(0,pq.remove());   // reversing the array list while adding in the list.e
+        }
+        return ans;
+    }
+}
