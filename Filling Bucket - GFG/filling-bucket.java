eca19d48a25540598181e3837d72faf6a035fb29
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-10-22 18:34:56 +0530

    Added solution - LeetHub

--- Filling Bucket - GFG/filling-bucket.java
+++ Filling Bucket - GFG/filling-bucket.java
@@ -23,43 +23,31 @@ class GFG {
 //User function Template for Java
 
 class Solution {
-
     static int fillingBucket(int N) {
         // code here
         if(N==1) return 1;
-        
-        long a =1;
+        long a=1;
         long b=1;
         long c=0;
-        long mod= 100000000;
-         for(int i =2; i<=N; i++)
-         {
-             c=((a%mod)+(b%mod) )%mod;
-             
-             a=b%mod;
-             b=c%mod;
-             
-         }
-         return (int)(c%mod);
-     
-    }
+        
+        long mod=100000000;
+        for( int i=2;i<=N ;i++)
+        {
+            
+            c = ((a%mod) +(b%mod))%mod;
+            a=b%mod;
+            b=c%mod;
+        }
+        return (int)(c%mod);
+    }  
 };
 
-
-//
-// static int fillingBucket(int N) {
-//         // code here
-        
-//         return fun(N);
+//  return fun(N);
 //     }
-    
-//  static int   fun (int N)
+//     static int  fun(int N)
 //     {
 //         if(N==0) return 1;
 //         if(N<0) return 0;
-       
-//       return  fun(N-1) + fun(N-2);
-//     } 
-    
-//     after writing this code- i got correct and but it was tle , then i checked for firther inputs and observed it was 
-//     Fibonacci serires
+        
+//         return fun(N-1)+fun(N-2);
+//     }
