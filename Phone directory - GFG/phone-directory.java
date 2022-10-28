25beec3a5a79bc29e7771d4c2c70b7b6ad903e1a
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-10-29 01:12:06 +0530

    Added solution - LeetHub

--- Phone directory - GFG/phone-directory.java
+++ Phone directory - GFG/phone-directory.java
@@ -34,79 +34,35 @@ class Solution{
                                         String contact[], String s)
     {
         // code here
-        trie root = new trie();
-        buildTrie(root, contact);
-        
-        //s*max(string.length)*nlogn
-        ArrayList<ArrayList<String>> ans  = new ArrayList<>();
-        for(int i =1; i<=s.length(); i++) //O(s)
+      ArrayList<ArrayList<String>> ans = new ArrayList<>();
+   
+        for(int i=1; i<=s.length(); i++)
         {
-            ArrayList<String> sub_ans =  new ArrayList<>();
-            find(s.substring(0,i) , root, sub_ans); // will append all poss distinct ans
-            
-            //sort it
+            Set<String> set  = new TreeSet<>();
+            String curr = s.substring(0,i);
+            for(int j =0; j<contact.length; j++)
+            {
+                if(contact[j].length() >= s.substring(0,i).length())
+                {
+                    String curr2 = contact[j].substring(0,i);
+                
+                    if(curr.equals(curr2))
+                    {
+                        set.add(contact[j]);
+                    }
+                }
+    
+            }
+            if(set.isEmpty())
+            {
+                set.add("0");
+            }
+               ArrayList<String> sub_ans = new ArrayList<>();
+               for(String x : set)
+               sub_ans.add(x);
             
             ans.add(sub_ans);
         }
         return ans;
-        
-    }
-    static void buildTrie(trie root, String c[])  // O(n*maxStrinlength)
-    {
-        trie temp = null;
-        for(int i=0 ;i<c.length; i++)
-        {
-            temp = root;
-            for(int j=0; j<c[i].length(); j++)
-            {
-                if(temp.ch[c[i].charAt(j) -'a'] ==null)
-                {
-                    temp.ch[c[i].charAt(j)- 'a'] = new trie();
-                }
-                temp =  temp.ch[c[i].charAt(j)- 'a'];
-                temp.set.add(c[i]);
-            }
-        }
-    }
-    
-    static void find(String s, trie root, ArrayList<String> sub_ans) // 
-    {
-        int flag=1;
-        
-        for(int i=0; i<s.length(); i++) //O(just traversing down in trie) O(S.lenth)
-        {
-            if(root.ch[s.charAt(i)-'a'] ==null)
-            
-            {
-                flag=0; //no substring with prefix(0,i-1)/ return 0
-                break;
-            }
-            root =  root.ch[s.charAt(i)-'a']; // if present just traverse
-                    
-        }
-        if(flag==0)
-        sub_ans.add("0");
-        
-        else
-        {
-            for(String x : root.set)
-            {
-                sub_ans.add(x);
-            }
-        }
-    }
-}
-
-class trie
-{
-    TreeSet<String> set;
-    trie ch[];
-    
-    trie()
-    {
-        set = new TreeSet<>();
-        ch = new trie[26];
-        for(int i=0; i<26;i++)
-        ch[i] = null;
     }
 }
