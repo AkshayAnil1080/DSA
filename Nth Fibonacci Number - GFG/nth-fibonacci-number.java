0327ede1593e5f0df7b7dc5b1b34c635c6a7f0d5
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-11-23 00:55:09 +0530

    Added solution - LeetHub

---
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            long n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+
+            System.out.println(ob.nthFibonacci(n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+class Solution {
+    static long nthFibonacci(long n){
+        // code here
+        long a =0; long b=1;
+        long c=0;
+        long mod = 1000000007;
+        for(int i=2; i<=n; i++){
+            c=(a%mod+b%mod)%mod;
+            a=b%mod;
+            b=c%mod;
+        }
+        return c%mod;
+    }
+}
