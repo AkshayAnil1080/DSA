8b7b75f1ae68ccc9d8a8ff9c949b228170a15eea
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-12-01 01:03:11 +0530

    Added solution - LeetHub

---
+++ Rearrange Array Alternately - GFG/rearrange-array-alternately.java
@@ -0,0 +1,87 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+	public static void main (String[] args)throws IOException
+	{
+
+// 		Scanner in = new Scanner(System.in);
+        
+// 		int t = in.nextInt();
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        
+         //testcases
+        int t = Integer.parseInt(read.readLine());
+		
+		while(t-- >0)
+		{
+		    //size of array
+		    int n = Integer.parseInt(read.readLine());
+		    long[] arr= new long[n];
+		    
+		    String str[] = read.readLine().trim().split(" ");
+		    
+		    //adding elements to the array
+		    for(int i=0;i<n;i++)
+		        arr[i] = Long.parseLong(str[i]);
+		    
+		    // StringBuffer sb = new StringBuffer();
+		    
+		    Solution ob = new Solution();
+		    
+		    //calling rearrange() function
+		    ob.rearrange(arr, n);
+		    StringBuffer sb = new StringBuffer();
+		    
+		    //appending and printing the elements
+		    for(int i =0; i < n; i++)
+		     sb.append(arr[i] + " ");
+		    System.out.println(sb);
+		}
+	}
+}
+
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // temp: input array
+    // n: size of array
+    //Function to rearrange  the array elements alternately.
+    public static void rearrange(long arr[], int n){
+        
+        // Your code here
+        long a[] = new long[n];
+        for(int i =0; i<n; i++)
+        a[i] = arr[i];
+        
+        int k=0;
+        for(int i =0; i<=n/2; i++)
+        {
+            //3 cases
+            if(n%2==0 && i==n/2)
+            continue;
+            
+            
+            if(n%2!=0 &&  i==n/2)
+            arr[k] = a[i];
+            
+            else
+            {
+                arr[k++] = a[n-i-1];
+                arr[k++] = a[i];
+            }
+        }
+        
+        
+    }
+    
+}
+
+
