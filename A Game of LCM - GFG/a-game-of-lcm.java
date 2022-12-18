6732604aa7bc0639a8fd0803cd898e799c9fceee
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2022-12-18 14:00:59 +0530

    Added solution - LeetHub

--- A Game of LCM - GFG/a-game-of-lcm.java
+++ A Game of LCM - GFG/a-game-of-lcm.java
@@ -20,35 +20,68 @@ class GFG {
 
 
 // User function Template for Java
+// class Solution {
+//     long maxGcd(int N) {
+//         // code here
+//         //s1 to find lcm of largest 4 poss num - n. n-1, n-2. n-3
+//         long x = (N*(N-1 )) / gcd(N,N-1);
+//         long y = ((N-2)*(N-3)) / gcd(N-2, N-3);
+        
+//         long z = (x*y)/gcd(x,y);
+        
+//         //s2 - a,b,c,d are co primes = gcd(a,b,c,d) =1 => lcm = a*b*c*d;
+//         long num = N*(N-1);  //we got a,b
+//         int count=0;
+//         for(int i = N-2; i>=1; i--) // to find c,d
+//         {
+//             if(gcd(num,i)==1)
+//             {
+//                 num *= i;
+//                 count++;
+//             }
+//             if(count==2) break;
+//         }
+//         return Math.max(z,num);
+        
+//     }
+//     long gcd(long a, long b){
+//         if(b==0)
+//         return a;
+        
+//         return gcd(b,a%b);
+//     }
+// }
 
 class Solution {
     long maxGcd(int N) {
         // code here
-        //s1 to find lcm of largest 4 poss num - n. n-1, n-2. n-3
-        long x = (N*(N-1 )) / gcd(N,N-1);
-        long y = ((N-2)*(N-3)) / gcd(N-2, N-3);
+        return Math.max(solve(N), solve(N-1));
+    }
+    long gcd(long a, long b ) // opt euclidean
+    {
+        if(b==0) return a;
+        return gcd(b, a%b);
+    }
+    long solve(int N)
+    {
+         long x = (N* (N-1))/gcd(N,N-1);
+        long y = ((N-2) * (N-3)) / gcd(N-2,N-3);
         
         long z = (x*y)/gcd(x,y);
         
-        //s2 - a,b,c,d are co primes = gcd(a,b,c,d) =1 => lcm = a*b*c*d;
-        long num = N*(N-1);  //we got a,b
+        //s2 need to compare with other approach
+        long num =(N *(N-1));
         int count=0;
-        for(int i = N-2; i>=1; i--) // to find c,d
+        for(int i=N-2; i>=1; i--)
         {
             if(gcd(num,i)==1)
             {
+                // System.out.println(i);
                 num *= i;
                 count++;
             }
             if(count==2) break;
         }
         return Math.max(z,num);
-        
-    }
-    long gcd(long a, long b){  //log N
-        if(b==0)
-        return a;
-        
-        return gcd(b,a%b);
     }
 }
