02c8a5a0eea108c4161cff4d7b0c32d36a9ed379
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-01-03 18:23:08 +0530

    Added solution - LeetHub

---
+++ Minimize number of Students to be removed - GFG/minimize-number-of-students-to-be-removed.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            int[] H = new int[N];
+            
+            for(int i=0; i<N; i++)
+                H[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            out.println(ob.removeStudents(H,N));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution {
+    public int removeStudents(int[] H, int N) {
+        
+        int[] dp = new int[H.length];
+        int len = 0;
+        for (int num : H) {
+            int idx = Arrays.binarySearch(dp, 0, len, num);
+            if (idx < 0) {
+                idx = -(idx + 1); // BS if ele not present retun -(desired pos + 1)
+            }
+            dp[idx] = num;
+            if (idx == len) { // if ele inserted is at last  0> increasem the size of lis
+                len++;
+            }
+        }
+        
+        return N-len;
+    }
+};
