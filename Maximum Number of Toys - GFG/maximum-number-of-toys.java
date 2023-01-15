7f884e1ed52b472851aa306ba5cb4938ab06c091
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-01-15 17:10:19 +0530

    Added solution - LeetHub

--- Maximum Number of Toys - GFG/maximum-number-of-toys.java
+++ Maximum Number of Toys - GFG/maximum-number-of-toys.java
@@ -70,68 +70,49 @@ class GFG {
 
 
 //User function Template for Java
+class Pair{
+    int price; int ind;
+    Pair(int price, int ind)
+    {
+        this.price= price;
+        this.ind = ind;
+    }
+}
 
 class Solution {
-    int fr[];
-    
-    class tree{
-        int N;
-        long bit[];
-        tree(int N){
-            this.N = N;
-            bit = new long[N];
-        }
-        
-        void add(int node, int v){
-            for(;node<N;node+=(node&(-node)))
-                bit[node]+=v;
-        }
-        
-        long get(int node){
-            long x = 0l;
-            for(;node>0;node-=(node&(-node)))
-                x+=bit[node];
-            return x;
-        }
-    }
-    
     ArrayList<Integer> maximumToys(int N, int A[], int Q, ArrayList<Integer> Queries[]){
-        ArrayList<Integer> res = new ArrayList<>();
-        fr = new int[1000 * 1000 + 5];
-        for(int x : A) fr[x]++;
-        tree obj1 = new tree(1000 * 1000 + 5);
-        tree obj2 = new tree(1000 * 1000 + 5);
-        for(int i=0;i<A.length;i++){
-          obj1.add(A[i],A[i]);
-          obj2.add(A[i],1);
+        
+        //s1 list of ele with its ind
+         ArrayList<Pair>  arr = new ArrayList<>();
+         for(int i =0; i<N; i++)
+        {
+            arr.add(new Pair(A[i],i));  // 0 based indexing
         }
-        for(ArrayList<Integer> i :Queries){
-            long C=i.get(0);
-            for(int j=2;j<i.size();j++){
-                obj1.add(A[i.get(j)-1],-A[i.get(j)-1]);
-                obj2.add(A[i.get(j)-1],-1);
-            }
-            int lw=1,hg=1000 * 1000;
-            int pos=1000 * 1000;
-            while(lw<=hg){
-                int mid=lw + (hg - lw)/2;
-                if(obj1.get(mid)>=C){
-                    pos=mid;
-                    hg=mid-1;
-                }
-                else{
-                    lw=mid+1;
-                }
-            }
-            long ans = obj2.get(pos - 1);
-            long mx=Math.min((C-obj1.get(pos-1))/pos,(long)fr[pos]);
-            ans+=mx;
-            res.add((int)ans);
-            for(int j=2;j<i.size();j++){
-                obj1.add(A[i.get(j)-1],A[i.get(j)-1]);
-                obj2.add(A[i.get(j)-1],1);
-            }
-        }
-        return res;
+         
+         // s2 sort it acc to ele
+         Collections.sort(arr, (a,b) -> a.price - b.price);
+         
+         //s3
+         ArrayList<Integer> ans = new ArrayList<>();
+         for(int i=0; i<Q; i++)
+         {
+             long money = Queries[i].get(0);
+             Set<Integer> broken  = new HashSet<>();
+             for(int j=2 ; j<Queries[i].size(); j++)
+             broken.add(Queries[i].get(j)-1);
+             
+             //buy toys greedily -> first which has themin value
+             int toys_bought=0;
+             for(int j=0; j<N && money >= arr.get(j).price; j++)    // runnning in sorted list
+             {
+                 if(!broken.contains(arr.get(j).ind))  //O(1))
+                 {
+                     money -= arr.get(j).price;
+                     toys_bought++;
+                 }
+             }
+             ans.add(toys_bought);
+         }
+         return ans;
     }
 }
