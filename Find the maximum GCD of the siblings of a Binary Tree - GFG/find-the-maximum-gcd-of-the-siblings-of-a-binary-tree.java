7ab56755f5096173150adb2111f8ae7e7961bbe3
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-01-17 01:03:35 +0530

    Added solution - LeetHub

---
+++ Find the maximum GCD of the siblings of a Binary Tree - GFG/find-the-maximum-gcd-of-the-siblings-of-a-binary-tree.java
@@ -0,0 +1,157 @@
+//{ Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+
+        while(t > 0){
+            String s = br.readLine();
+	    	Node root = buildTree(s);
+	    	
+            Solution ob = new Solution();
+    		System.out.println(ob.maxGCD(root));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    int max_gcd =1; int ans=0;
+    int maxGCD(Node root) {
+        // code here 
+        
+        if(root==null)
+        return -1;
+        
+        pre(root);
+        return ans;
+        
+    }
+    void pre(Node root)
+    {
+        if(root==null)
+        return;
+        
+        if(root.right!=null && root.left!=null)
+        {
+            int curr_gcd = gcd(root.left.data, root.right.data);
+            if(curr_gcd > max_gcd)
+            {
+                max_gcd = curr_gcd;
+                ans=root.data;
+            }
+            else if(curr_gcd == max_gcd)
+            ans = Math.max(ans, root.data);
+        }
+        pre(root.left);
+        pre(root.right);
+    }
+    
+    int gcd(int a, int b)
+    {
+        if(b==0)
+        return a;
+        
+        return gcd(b, a%b);
+    }
+}
+
