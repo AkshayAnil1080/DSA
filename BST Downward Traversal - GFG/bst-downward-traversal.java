2b2dc9e306ad0fd38a9b0321d96d95fdd115bfe0
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-02-06 02:27:00 +0530

    Added solution - LeetHub

---
+++ BST Downward Traversal - GFG/bst-downward-traversal.java
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        PrintWriter out=new PrintWriter(System.out);
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+    	    	int target=Integer.parseInt(br.readLine());
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+        	    long ans=g.verticallyDownBST(root,target);
+        	    out.println(ans);
+                t--;
+            
+        }
+        out.close();
+    }
+  
+}
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    long ans=0;
+    Node tar;
+    long verticallyDownBST(Node root,int target)
+    {
+        //s1 - search the target node in bst
+        while(root!=null)
+        {
+            if(root.data < target) 
+            {
+                root=  root.right;
+            }
+            else if(root.data > target)
+            {
+                root = root.left;
+            }
+            else //==
+            break;
+        }
+        if(root==null)
+        return -1;
+        
+        tar = root;
+        inorder(tar, 0);
+        return ans;
+        
+    }
+    void inorder(Node node, int dir)
+    {
+       if(node!=null)
+       {
+           inorder(node.left, dir-1);
+           inorder(node.right, dir+1);
+           if(node!=tar && dir==0)
+           ans+=node.data;
+       }
+    }
+}
