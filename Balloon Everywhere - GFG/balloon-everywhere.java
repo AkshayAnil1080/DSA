0ce44b9806d1aa3fa31d9b3f5d336b48918bc0dc
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-02-10 01:15:32 +0530

    Added solution - LeetHub

--- Balloon Everywhere - GFG/balloon-everywhere.java
+++ Balloon Everywhere - GFG/balloon-everywhere.java
@@ -26,25 +26,24 @@ class GFG {
 
 
 // User function Template for Java
+
 class Solution {
     public int maxInstance(String s) {
         // Code here
+        
         Map<Character, Integer> map = new HashMap<>();
         for(int i=0; i<s.length(); i++)
         {
             char ch = s.charAt(i);
-            if(ch=='a' ||  ch=='b' || ch=='l' || ch=='o' || ch=='n')
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            if(ch=='a' || ch=='b' ||ch=='l' ||ch=='o' ||ch=='n' )
+            map.put(ch, map.getOrDefault(ch,0)+1);
         }
-        map.put( 'l',  map.get('l')/2);
-         map.put( 'o',  map.get('o')/2);
+        map.put('l',map.get('l')/2);  map.put('o',map.get('o')/2);
         int ans= Integer.MAX_VALUE;
-        for(char x : map.keySet())
-        {
-           
-            ans = Math.min(map.get(x), ans);
-            
-        }
+        
+        for(char x: map.keySet())
+        ans= Math.min(map.get(x), ans);
+    
         return ans;
-     }
+    }
 }
