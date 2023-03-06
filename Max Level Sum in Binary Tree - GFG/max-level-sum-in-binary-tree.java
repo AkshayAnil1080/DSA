bc00fe373f86a8adac9ffded312dfb0fc185763e
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-03-07 00:32:13 +0530

    Added solution - LeetHub

---
+++ Max Level Sum in Binary Tree - GFG/max-level-sum-in-binary-tree.java
@@ -0,0 +1,126 @@
+//{ Driver Code Starts
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Main {
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String s = br.readLine().trim();
+            Node root = buildTree(s);
+
+            Solution g = new Solution();
+            System.out.println(g.maxLevelSum(root));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*Complete the finction below
+Node is as follows
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}*/
+class Solution {
+    public int maxLevelSum(Node root) {
+        // add code here.
+         Queue<Node> q = new LinkedList<>();  //s1
+        q.add(root);
+        
+        int ans = Integer.MIN_VALUE;
+        while(!q.isEmpty()) //s2
+        {
+            int n = q.size();
+            int sum=0; // maintain the sum of nodes each each level
+            for(int i=0; i<n ;i++) //s3 itr in nodes of each level
+            {
+                Node curr = q.poll();
+                sum += curr.data;
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+                
+            }
+            ans = Math.max(ans, sum);  // after each level , track the the maximum sum.
+        }
+        return ans;
+    }
+}
