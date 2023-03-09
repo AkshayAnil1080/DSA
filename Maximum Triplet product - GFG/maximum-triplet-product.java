b48f7fb76de476e0d769b75e3bb169c3f4c23200
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-03-10 01:10:41 +0530

    Added solution - LeetHub

--- Maximum Triplet product - GFG/maximum-triplet-product.java
+++ Maximum Triplet product - GFG/maximum-triplet-product.java
@@ -14,24 +14,38 @@ class Solution {
     Long maxTripletProduct(Long arr[], int n)
     {
         // Complete the function
-         // Complete the function
-        PriorityQueue<Long>  max_h = new PriorityQueue<Long>(Collections.reverseOrder());
-        PriorityQueue<Long>  min_h = new PriorityQueue<Long>();
+        long m1=Integer.MIN_VALUE , m2=Integer.MIN_VALUE ,m3=Integer.MIN_VALUE;
         
-        for(int i=0; i<n; i++)
+        long min1 =Long.MAX_VALUE, min2 = Integer.MAX_VALUE;
+       
+        for(int i = 0; i < n; i++)
         {
-            max_h.add(arr[i]); min_h.add(arr[i]);
+              
+            if (arr[i] > m1)
+            {
+               m3=m2; m2=m1; m1=arr[i];
+            }
+       
+            else if (arr[i] > m2)
+            {
+                m3=m2; m2=arr[i];
+            }
+       
+            else if (arr[i] > m3)
+                m3=arr[i];
+       
+           
+            if (arr[i] < min1)
+            {
+               min2 = min1;  min1 =arr[i];
+            }
+            else if(arr[i] < min2)
+                min2 = arr[i];
         }
         
-        long m1  = max_h.remove();
-          long m2  = max_h.remove();
-            long m3  = max_h.remove();
-            
-        long min1 = min_h.remove();
-         long min2 = min_h.remove();
-         
-         long ans = Math.max(m1*m2*m3, min1*min2*m1);
-         return ans;
+        long ans = (long)Math.max(min1 * min2 * m1, m1*m2*m3);
+        
+        return ans;
     }
 }
 
