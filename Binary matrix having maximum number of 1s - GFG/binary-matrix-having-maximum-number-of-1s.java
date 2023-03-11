fc184476c6b67c10fbc8164873d47ddb9a861ac2
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-03-12 01:10:31 +0530

    Added solution - LeetHub

---
+++ Binary matrix having maximum number of 1s - GFG/binary-matrix-having-maximum-number-of-1s.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class FastReader{ 
+    BufferedReader br; 
+    StringTokenizer st; 
+
+    public FastReader(){ 
+        br = new BufferedReader(new InputStreamReader(System.in)); 
+    } 
+
+    String next(){ 
+        while (st == null || !st.hasMoreElements()){ 
+            try{ st = new StringTokenizer(br.readLine()); } catch (IOException  e){ e.printStackTrace(); } 
+        } 
+        return st.nextToken(); 
+    } 
+
+    String nextLine(){ 
+        String str = ""; 
+        try{ str = br.readLine(); } catch (IOException e) { e.printStackTrace(); } 
+        return str; 
+    } 
+    
+    Integer nextInt(){
+        return Integer.parseInt(next());
+    }
+} 
+
+class GfG {
+    public static void main(String[] args) {
+        FastReader sc = new FastReader();
+        PrintWriter out = new PrintWriter(System.out);
+        int t = sc.nextInt();
+        while(t-- > 0) {
+            int B = sc.nextInt();
+            int arr[][] = new int[B][B];
+            for(int i = 0; i < B; i++) {
+                for(int j = 0; j < B; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+            Solution obj = new Solution();
+            int ans[] = obj.findMaxRow(arr, B);
+            for(int i = 0; i < 2; i++) {
+                out.print(ans[i] + " ");
+            }
+            out.println();
+        }
+        out.flush();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    public int[] findMaxRow(int mat[][], int N) {
+        // code here
+        int res[] = new int[2];
+
+        int ans=0; int row=0;
+        for(int i=0; i<N; i++)
+        {
+            int count=0;
+            for(int j=0; j<N; j++)
+            {
+                if(mat[i][j]==1)
+                count++;
+            }
+          
+            if(count>ans)
+            {
+                ans=count; row=i;
+            }
+            
+        }
+        res[0]=row; res[1]=ans;
+        return res;
+    }
+};
