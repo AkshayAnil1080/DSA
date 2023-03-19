3d12aea750728a90500b58ec20d960d86dd335dc
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-03-20 01:56:11 +0530

    Added solution - LeetHub

--- Shortest XY distance in Grid - GFG/shortest-xy-distance-in-grid.java
+++ Shortest XY distance in Grid - GFG/shortest-xy-distance-in-grid.java
@@ -31,42 +31,105 @@ class GFG {
 }
 // } Driver Code Ends
 
-
-// User function Template for Java
-class Pair{
-    int a; int b;
-    Pair(int a, int b)
-    {
-        this.a=a; this.b=b;
-    }
-}
-
 class Solution {
-    static int shortestXYDist(ArrayList<ArrayList<Character>> grid, int N, int M) {
-        // code here
-         ArrayList<Pair> x = new ArrayList<>();
-         ArrayList<Pair> y = new ArrayList<>();
-        for(int i=0; i<N; i++)
-        {
-            for(int j=0; j<M; j++)
-            {
-                if(grid.get(i).get(j)=='X')
-                x.add(new Pair(i,j));
-                
-                if(grid.get(i).get(j)=='Y')
-                y.add(new Pair(i,j));
+    static int shortestXYDist(ArrayList<ArrayList<Character>> grid, int N,
+                              int M) {
+        int[][] dist = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                dist[i][j] = Integer.MAX_VALUE-1;
             }
         }
-        int ans=Integer.MAX_VALUE;
-        for(int i=0; i<x.size(); i++)
-        {
-            for(int j=0; j<y.size();j++)
-            {
-                ans = Math.min(ans , Math.abs(x.get(i).a - y.get(j).a) + Math.abs(x.get(i).b - y.get(j).b)); ;
+
+       
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (grid.get(i).get(j) == 'X')
+                    dist[i][j] = 0;
+                else {
+                    if (i-1 >= 0)  //top
+                        dist[i][j] = Math.min(dist[i][j], dist[i - 1][j] + 1);
+                    if(j+1<M) //right --
+                         dist[i][j] = Math.min(dist[i][j], dist[i][j + 1] + 1);
+                    if(i+1<N)//bottom --
+                         dist[i][j] = Math.min(dist[i][j], dist[i+1][j] + 1);
+                    if (j-1>=0) //left
+                        dist[i][j] = Math.min(dist[i][j], dist[i][j - 1] + 1);
+                      
+                }
             }
         }
+        
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = M - 1; j >= 0; j--) {
+                if (grid.get(i).get(j) == 'X')
+                    dist[i][j] = 0;
+                else {
+                    if (i-1 >= 0)  //top
+                        dist[i][j] = Math.min(dist[i][j], dist[i - 1][j] + 1);
+                    if(j+1<M) //right --
+                         dist[i][j] = Math.min(dist[i][j], dist[i][j + 1] + 1);
+                    if(i+1<N)//bottom --
+                         dist[i][j] = Math.min(dist[i][j], dist[i+1][j] + 1);
+                    if (j-1>=0) //left
+                        dist[i][j] = Math.min(dist[i][j], dist[i][j - 1] + 1);
+                      
+                }
+            }
+        }
+        
+        
+        int ans = Integer.MAX_VALUE-1;
+             for (int i = 0; i < N; i++) 
+             {
+                for (int j = 0; j < M; j++) 
+                {
+                    if (grid.get(i).get(j) == 'Y') ans = Math.min(ans, dist[i][j]);
+                }
+            }
+
         return ans;
-        
-        
     }
 };
+
+
+// User function Template for Java
+// class Pair{
+//     int a; int b;
+//     Pair(int a, int b)
+//     {
+//         this.a=a; this.b=b;
+//     }
+// }
+
+// class Solution {
+//     static int shortestXYDist(ArrayList<ArrayList<Character>> grid, int N, int M) {
+//         // code here
+//          ArrayList<Pair> x = new ArrayList<>();
+//          ArrayList<Pair> y = new ArrayList<>();
+//         for(int i=0; i<N; i++)
+//         {
+//             for(int j=0; j<M; j++)
+//             {
+//                 if(grid.get(i).get(j)=='X')
+//                 x.add(new Pair(i,j));
+                
+//                 if(grid.get(i).get(j)=='Y')
+//                 y.add(new Pair(i,j));
+//             }
+//         }
+//         int ans=Integer.MAX_VALUE;
+//         for(int i=0; i<x.size(); i++)
+//         {
+//             for(int j=0; j<y.size();j++)
+//             {
+//                 ans = Math.min(ans , Math.abs(x.get(i).a - y.get(j).a) + Math.abs(x.get(i).b - y.get(j).b)); ;
+//             }
+//         }
+//         return ans;
+        
+        
+//     }
+// };
+
