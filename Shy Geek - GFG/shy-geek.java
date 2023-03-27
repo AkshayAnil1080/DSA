968e52e51fe91ffe80d0a6b68d15831547d34437
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-03-28 04:23:01 +0530

    Added solution - LeetHub

---
+++ Shy Geek - GFG/shy-geek.java
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static BufferedReader br;
+    static PrintWriter ot;
+    public static void main(String[] args) throws IOException{
+        
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+
+        int t = Integer.parseInt(br.readLine());
+        
+
+        while(t-->0){
+            Shop shop = new Shop();
+
+            String s[] = br.readLine().trim().split(" ");
+            
+            int n = Integer.parseInt(s[0]);
+            long k = Long.parseLong(s[1]);
+
+            s = br.readLine().trim().split(" ");
+
+            for(String x: s){
+                shop.addChocolate(Integer.parseInt(x));
+            }
+            
+            ot.println(new Solution(shop).find(n, k));
+        }
+        ot.close();
+    }
+    
+    
+}
+class Shop{
+    private static List<Integer> chocoloates;
+    private static int countOfCalls;
+    Shop(){
+        chocoloates = new ArrayList<>();
+        countOfCalls = 0;
+    }
+    public static void addChocolate(int price){
+        chocoloates.add(price);
+    }
+
+    public static int get(int i){
+        countOfCalls++;
+        if(countOfCalls > 50 || i >= chocoloates.size() || i < 0)
+            return -1;
+        return chocoloates.get(i);
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+Instructions - 
+
+    1. 'shop' is object of class Shop.
+    2. User 'shop.get(int i)' to enquire about the price
+            of the i^th chocolate.
+    3. Every chocolate in shop is arranged in increasing order
+            i.e. shop.get(i) <= shop.get(i + 1) for all 0 <= i < n - 1
+*/
+class Solution{
+    
+    static Shop shop;
+    Solution(Shop shop){
+        this.shop = shop;
+    }
+    static long find(int n, long k){
+        // Return the number of chocolates Geek had
+        // enjoyed out of 'n' chocolates availabe in the
+        // 'shop'.
+        
+        int l = 0, h = n - 1;
+        int count = 0;
+      
+        
+         Map<Integer, Long> hm = new HashMap<>();
+        while(k > 0)
+        {
+            l = 0;
+            while(l <= h){
+                int mid = l +(h-l)/2;
+                long temp;
+                
+                if(hm.containsKey(mid))
+                    temp = hm.get(mid);
+                else
+                    {
+                        temp =shop.get(mid);
+                         hm.put(mid, temp);
+                    }
+                
+                if(temp <= k)
+                    l = mid + 1;
+                else 
+                    h = mid - 1;
+            }
+            if(h == -1)
+                break;
+            count += (k/hm.get(h));
+            
+            k=  k%hm.get(h);
+           
+            
+        }
+        return count;
+    }
+
+}
