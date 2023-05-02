fe8953b9c0f990c20985a22357e8cf78b93a5b7e
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-05-03 02:17:24 +0530

    Added solution - LeetHub

--- Make Palindrome - GFG/make-palindrome.java
+++ Make Palindrome - GFG/make-palindrome.java
@@ -66,43 +66,44 @@ class Solution {
         {
             String curr =  e.getKey();  int freq = e.getValue();    
             //if palin,  // put it in another map and check freq not be odd
-            if(isPalin(curr))
+             if(isPalin(curr))  // put it in another map and check freq not be odd
             {
                 temp.put(curr,freq);
                 continue;
             }
-            //if not palin, check if its reversal is present with same freq  
-            StringBuilder sb=new StringBuilder(curr);
-            sb.reverse();  //O(len(arr[i]))
+            
+            //if not palin, check if its reversal is present with same freq    
+             StringBuilder sb=new StringBuilder(curr);
+            sb.reverse();  // O(len(arr[i]))
             int y= map.get(sb.toString())==null ? 0 :  map.get(sb.toString()); // else it takes null values;
             
-             if(y==0 || freq!=y) // reversal is not present, reversal is present with diff freq
+            if(y==0 || freq!=y) // reversal is not present, reversal is present with diff freq
                {
                  return false;
                }
         }
-            int cnt=0; 
-            for(int i:temp.values())
-            {
-                if(i%2==1)    cnt++;
-                
-            }
-            if(cnt>1)  return false;
-            return true;
-            // not more than 1 palindrimic string should have freq in odd number ->ret false.
-        }
-         public static boolean isPalin(String s)
+        
+         int cnt=0; // cnt of palindromic string with freq as odd, agar ek v aise hua to not poss
+        for(int i:temp.values())
         {
-            int i=0,j=s.length()-1;
-            while(i<j){
-                if(s.charAt(i)!=s.charAt(j)){
-                    return false;
-                }
-                i++;
-                j--;
-            }
-            return true;
+            if(i%2==1)    cnt++;
+            
         }
+        if(cnt>1)  return false;
+        return true;
+        // not more than 1 palindrimic string should have freq in odd number ->ret false.
     }    
-    
+     public static boolean isPalin(String s)
+    {
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
         
