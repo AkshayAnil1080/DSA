72138801f6821cf8801609810a093313e1785786
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-05-27 18:39:14 +0530

    Added solution - LeetHub

--- Modify Linked List-1 - GFG/modify-linked-list1.java
+++ Modify Linked List-1 - GFG/modify-linked-list1.java
@@ -43,33 +43,60 @@ class GFG{
 
 
 //User function Template for Java
+
 class Solution
 {
     public static Node modifyTheList(Node head)
     {
-        //make array and ele in list
-        ArrayList<Integer> al = new ArrayList<>();
-        Node curr = head;
+     Node mid = get_m(head);
+     
+     //partion 2nd list
+     Node backlist = mid.next;  // 3->6 -> N
+     
+     //mark next as null for last node of first list
+     mid.next=null;  //10 > 4 > 5> N
+     
+     backlist = reverse(backlist);  //6 >3 >N
+     
+     //itr in both list , store diff in 1st and copy 1st list data to 2nd
+     Node curr1 = head; Node curr2 = backlist;
+     while(curr2!=null)
+     {
+         int temp = curr1.data;
+         curr1.data = curr2.data-temp;
+         curr2.data = temp;
+         
+         curr1 = curr1.next; curr2=curr2.next;
+     }
+    //  1st : -4, -1, 5 -> N  || 2nd : 10>4>N
+     //3. reverse 2nd list and append in 1st(last node of 1st list is mid)
+     mid.next  = reverse(backlist);
+     return head;
+    }
+   static Node get_m(Node head)
+    {
+        Node f=head;
+        Node s=head;
+        while(f.next!=null && f.next.next!=null)
+        {
+            s=s.next;
+            f=f.next.next;
+        }
+        return s;
+    }
+   static Node reverse(Node head)
+    {
+        Node prev=null;
+        Node curr=head;
+        Node next=null;
         while(curr!=null)
         {
-            al.add(curr.data);
-            curr=curr.next;
+            next = curr.next;
+            curr.next=prev;
+            prev=curr;
+            
+            curr=next; // repeat the above three lines for next nodes;
         }
-        int n = al.size();
-         curr=head;
-        for(int i=0; i<n/2; i++)
-        {
-            int x =   al.get(n-i-1) - al.get(i);
-           al.set(n-i-1,al.get(i));
-           
-           al.set(i,x);
-        }
-        int i =0; 
-        while(curr!=null)
-        {
-            curr.data = al.get(i++);
-            curr=curr.next;
-        }
-        return head;
+        return prev;
     }
 }
