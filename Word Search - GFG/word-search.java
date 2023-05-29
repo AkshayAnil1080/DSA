09bcbbf204206189dee963a5a145c23a426cef15
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-05-30 02:09:17 +0530

    Added solution - LeetHub

--- Word Search - GFG/word-search.java
+++ Word Search - GFG/word-search.java
@@ -35,35 +35,38 @@ class GFG
 
 class Solution
 {
-    boolean adjacentSearch(char[][] board, String word, int i, int j, int len)
-    {
-        if(len == word.length())
-            return true;
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length)
-            return false;
-        if(board[i][j] != word.charAt(len))
-            return false;
-            //character mactches, mark it and recur for all 4 dir for next ch.
-        board[i][j] = '*';
-        boolean ans = 
-            adjacentSearch(board, word, i-1, j, len+1) || //up
-            adjacentSearch(board, word, i+1, j, len+1) || //down
-            adjacentSearch(board, word, i, j-1, len+1) || //left
-            adjacentSearch(board, word, i, j+1, len+1);   // right
-        // board[i][j] = word.charAt(len);// i cannot find complete word in this path, reset the path and return to parent call.
-        return ans;
-    }
-    
     public boolean isWordExist(char[][] board, String word)
     {
+        // Code here
         for(int i = 0; i < board.length; i++)
         {
             for(int j = 0; j < board[0].length; j++)
             {
-                if(board[i][j] == word.charAt(0) && adjacentSearch(board, word, i, j, 0))
+                if(board[i][j] == word.charAt(0) && help(board, word, i, j, 0))
                     return true;
             }
         }
         return false;
     }
+     boolean help(char[][] b, String w, int i, int j, int len)
+    {
+        if(len == w.length())  //if reached the word end
+            return true;
+            
+        if(i < 0 || j < 0 || i >= b.length || j >= b[0].length) //if went oob
+            return false;
+            
+        if(b[i][j] != w.charAt(len)) //the dir in which we moved does not have matching ch.
+            return false;
+            //character mactches, mark it and recur for all 4 dir for next ch.
+            
+        b[i][j] = '*';  //mark it vis so that it cannot take the same ch again in path.
+       return
+            help(b, w, i-1, j, len+1) || //up
+           help(b, w, i+1, j, len+1) || //down
+            help(b, w, i, j-1, len+1) || //left
+            help(b, w, i, j+1, len+1);   // right
+   
+    }
+    
 }
