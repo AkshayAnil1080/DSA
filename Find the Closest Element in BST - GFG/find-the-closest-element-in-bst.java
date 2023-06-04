e5d8876115062395dbeb0600195e39470708a78a
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-05 02:07:32 +0530

    Added solution - LeetHub

--- Find the Closest Element in BST - GFG/find-the-closest-element-in-bst.java
+++ Find the Closest Element in BST - GFG/find-the-closest-element-in-bst.java
@@ -115,24 +115,22 @@ class Solution
 {
     //Function to find the least absolute difference between any node
     //value of the BST and the given integer.
-    static int ans;
     static int minDiff(Node  root, int K) 
     { 
-        // Write your code here
-        ans=Integer.MAX_VALUE;
-        ino(root,K);
-        return ans;
+       return help(root, K);
     } 
-    static void ino(Node root,int k)
+    static int help(Node root, int k)
     {
-        if(root!=null)
-       {
+        if(root==null) return Integer.MAX_VALUE;
         
-         
-        ino(root.left,k);
-        ans = Math.min(ans, Math.abs(k-root.data));  //processing
-        ino(root.right, k);
-       }
+         if(root.data==k) //if node is fonnd return 0;
+            return 0;
         
+        
+        if(root.data>k)
+            return Math.min( root.data-k ,help(root.left,k) );
+        
+        //else we call the function recursively for the right subtree.
+        return Math.min( k - root.data , help(root.right,k) );
     }
 }
