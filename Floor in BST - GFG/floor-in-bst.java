4444a139e54b61b2427b244ba1d5e564d77470bc
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-06 01:48:18 +0530

    Added solution - LeetHub

---
+++ Floor in BST - GFG/floor-in-bst.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class Node {
+    int data;
+    Node right;
+    Node left;
+
+    Node(int val) {
+        data = val;
+        right = null;
+        left = null;
+    }
+}
+
+class GFG {
+
+    public static Node insert(Node tree, int val) {
+        Node temp = null;
+        if (tree == null) {
+            return new Node(val);
+        }
+
+        if (val < tree.data) {
+            tree.left = insert(tree.left, val);
+        } else if (val > tree.data) {
+            tree.right = insert(tree.right, val);
+        }
+
+        return tree;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            Node root = null;
+            int n = sc.nextInt();
+            for (int i = 0; i < n; i++) {
+                int k = sc.nextInt();
+                root = insert(root, k);
+            }
+
+            int s = sc.nextInt();
+
+            Solution obj = new Solution();
+            int ans = obj.floor(root, s);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static int floor(Node root, int x) {
+        // Code here
+        
+        int res = -1;
+    while(root!=null)
+    {
+        if(root.data==x)return root.data;
+        
+        if(root.data>x)
+        {
+          root=root.left;
+        }
+        
+        else
+        {  res=root.data;
+            root=root.right;
+        }
+    }
+    return res;
+    }
+}
