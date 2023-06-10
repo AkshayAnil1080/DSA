7ab55c3cb1ef39eab0f6948849c91e2089ca4354
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-11 03:32:34 +0530

    Added solution - LeetHub

--- Adding Ones - GFG/adding-ones.java
+++ Adding Ones - GFG/adding-ones.java
@@ -49,11 +49,19 @@ class Solution {
     public static void update(int a[], int n, int update[], int k)
     {
         // Your code goes here
-         for(int i=0; i<k; i++)
-         {
-             for(int j= update[i]-1 ;j<n ;j++)
-             a[j]++;
-         }
+         //   N*K
+        //  for(int i=0; i<k; i++)
+        //  {
+        //      for(int j= update[i]-1 ;j<n ;j++)
+        //      a[j]++;
+        //  }
+         
+         // O(k+N)
+        for(int i=0; i<k; i++)
+        a[update[i]-1]++;
+        
+          for(int i=1; i<n; i++)
+            a[i]+=a[i-1];
     }
     
 }
