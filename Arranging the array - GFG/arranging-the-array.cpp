d0b2c9d4d7039986ced9cec6bc6038937df59984
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-10 15:30:28 +0530

    Added solution - LeetHub

--- Arranging the array - GFG/arranging-the-array.cpp
+++ Arranging the array - GFG/arranging-the-array.cpp
@@ -11,61 +11,42 @@ class Solution
     public:
         void Rearrange(int arr[], int n)
         {
-              mergeSort(arr, 0, n-1);
+               mergeSort(arr, 0, n-1);
     }
-    void mergeSort(int arr[], int l, int r)
-    {
+    
+
+    void mergeSort(int arr[], int l, int r) {
         if (l < r) {
-            // Same as (l + r)/2, but avoids overflow for
-            // large l and h
             int m = l + (r - l) / 2;
-     
-            // Sort first and second halves
-           mergeSort(arr, l, m);
+            mergeSort(arr, l, m);
             mergeSort(arr, m + 1, r);
-     
             merge(arr, l, m, r);
         }
     }
-    void merge(int arr[], int l, int m, int r)
-{
-    int i, j, k;
-    int n1 = m - l + 1;
-    int n2 = r - m;
- 
-   
-      int L[n1] ;
-        int R[n2]; 
-  for (i = 0; i < n1; i++)
-        L[i] = arr[l + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[m + 1 + j];
-        
- 
-  
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
- 
     
- 
-   
-    while (i < n1 && L[i] < 0)
-        arr[k++] = L[i++];
- 
-    while (j < n2 && R[j] < 0)
-        arr[k++] = R[j++];
- 
-    while (i < n1)
-        arr[k++] = L[i++];
- 
-    while (j < n2)
-        arr[k++] = R[j++];
-}
+    void merge(int arr[], int l, int m, int r) {
+        int i = l;
+        int j = m + 1;
+
+        while (i <= m && arr[i] < 0)
+            i++;
+
+        while (j <= r && arr[j] < 0)
+            j++;
+
+        reverse(arr, i, m);
+        reverse(arr, m + 1, j - 1);
+        reverse(arr, i, j - 1);
+    }
+    
+    void reverse(int arr[], int l, int r) {
+        while (l < r) {
+            swap(arr[l], arr[r]);
+            l++;
+            r--;
+        }
+    }
 };
-
-
-
 //{ Driver Code Starts.
 void Rearrange(int arr[], int n);
 
