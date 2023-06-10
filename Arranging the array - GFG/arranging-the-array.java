1dff6d3278e9377a81a18f8c9b00b3a264884418
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-10 15:23:07 +0530

    Added solution - LeetHub

--- Arranging the array - GFG/arranging-the-array.java
+++ Arranging the array - GFG/arranging-the-array.java
@@ -37,27 +37,66 @@ class GFG {
 // } Driver Code Ends
 
 
-//User function Template for Java
+
+
+
 class Solution {
     
     public void Rearrange(int a[], int n)
     {
         // Your code goes here
-        int temp[] = new int[n];
-        int k=0;
-        for(int i=0; i<n ;i++)
-        if(a[i]<0)
-        {
-            temp[k++]=a[i];
-        }
-        for(int i=0; i<n; i++)
-        {
-            if(a[i]>=0)
-            temp[k++]=a[i];
-        }
-        
-        for(int i=0; i<n; i++)
-        a[i]=temp[i];
-    
+        mergeSort(a, 0, n-1);
     }
+    void mergeSort(int arr[], int l, int r)
+    {
+        if (l < r) {
+            // Same as (l + r)/2, but avoids overflow for
+            // large l and h
+            int m = l + (r - l) / 2;
+     
+            // Sort first and second halves
+           mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+     
+            merge(arr, l, m, r);
+        }
+    }
+
+
+    void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+   
+      int L[] = new int[n1];
+        int R[] = new int[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+        
+ 
+  
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+ 
+    
+ 
+   
+    while (i < n1 && L[i] < 0)
+        arr[k++] = L[i++];
+ 
+    while (j < n2 && R[j] < 0)
+        arr[k++] = R[j++];
+ 
+    while (i < n1)
+        arr[k++] = L[i++];
+ 
+    while (j < n2)
+        arr[k++] = R[j++];
+}
+ 
 }
