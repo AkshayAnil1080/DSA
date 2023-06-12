82ad117ce2ed10cf87373cf98e1d4e064bb86646
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-13 00:59:41 +0530

    Added solution - LeetHub

---
+++ k largest elements - GFG/k-largest-elements.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int[] ans = new Solution().kLargest(arr, n, k);
+            for (int x : ans) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    int[] kLargest(int[] arr, int n, int k) {
+        
+        // mm2
+        PriorityQueue<Integer> pq = new PriorityQueue<>(); //min heap
+        for(int i=0; i<k; i++)
+        {
+            pq.add(arr[i]);
+        }
+        for(int i=k; i<n ;i++)
+        {
+            if(arr[i] >= pq.peek())
+            {
+                pq.remove();
+                pq.add(arr[i]);
+            }
+        }
+        ArrayList<Integer> al = new ArrayList<>();
+         
+          for( int i=0;i<k;i++)
+            al.add(pq.remove());
+        Collections.sort(al, Collections.reverseOrder());
+        
+        int a[] = new int[k];
+        for(int i=0; i<k; i++)
+        a[i] = al.get(i);
+        
+        return a;
+    }
+}
+
+
+// code here
+        //m1 O(N+klogn)
+        //  int a[] =  new int[k];
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());/max
+        
+        // for( int i = 0;i<n;i++)
+        // pq.add(arr[i]);
+        
+        // for( int i=0;i<k;i++)
+        // a[i] = pq.remove();
+        
+        // return a;
+        
