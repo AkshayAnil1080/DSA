c45f475c2284dd490594e763b812f94cd6685222
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-17 03:54:18 +0530

    Added solution - LeetHub

---
+++ Queue Operations - GFG/queue-operations.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+// Helper class Geeks to implement 
+// insert() and findFrequency()
+class Geeks{
+    
+    // Function to insert element into the queue
+    static void insert(Queue<Integer> q, int k){
+        
+        // Your code here
+        q.add(k);
+    }
+    
+    // Function to find frequency of an element
+    // rteturn the frequency of k
+    static int findFrequency(Queue<Integer> q, int k){
+        
+        // Your code here
+        Queue<Integer> c_q = new LinkedList<>();
+        int ans=0;
+        while(!q.isEmpty())
+        {
+            int curr = q.poll();
+            c_q.add(curr);
+            if(curr==k)
+            ans++;
+        }
+         while(!c_q.isEmpty())
+         q.add(c_q.poll());
+        
+        return ans;
+        
+        
+    }
+    
+}
+
+//{ Driver Code Starts.
+
+// Driver class with driver code
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) {
+		
+		// Taking input using scanner class
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		
+		while(testcase-- > 0){
+		    // Declaring Queue
+		    Queue<Integer> q = new LinkedList<>();
+		    int n = sc.nextInt();
+		    
+		    // Invoking object of Geeks class
+		    Geeks obj = new Geeks();
+		    
+		    for(int i = 0;i<n;i++){
+		        int k = sc.nextInt();
+		        obj.insert(q, k);
+		    }
+		   
+		    int x = sc.nextInt();
+		    for(int i = 0;i<x;i++){
+		        int k = sc.nextInt();
+		        int f = obj.findFrequency(q, k);
+		        if(f != 0){
+		            System.out.println(f);
+		        }
+		        else{
+		            System.out.println("-1");
+		        }
+		    }
+		}
+	}
+}
+// } Driver Code Ends
