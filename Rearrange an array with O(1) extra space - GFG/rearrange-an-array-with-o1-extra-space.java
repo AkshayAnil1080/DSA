35af58ca4d7c4e0527710fca99044c47c41e0fb2
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-19 01:15:13 +0530

    Added solution - LeetHub

---
+++ Rearrange an array with O(1) extra space - GFG/rearrange-an-array-with-o1-extra-space.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        //testcases
+        int t =Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            //size of array
+            int n = Integer.parseInt(read.readLine());
+            String st[] = read.readLine().trim().split("\\s+");
+            long arr[] = new long[(int)n];
+            
+            //adding elements to the array
+            for(int i = 0; i < n; i++)
+                arr[(int)i]  =Integer.parseInt(st[(int)i]);
+                
+           //calling arrange() function
+           new Solution().arrange(arr, n);
+           
+           //printing the elements 
+           for(int i = 0; i < n; i++)
+            System.out.print(arr[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+ 
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    static void arrange(long arr[], int n)
+    {
+        // your code here
+        long temp[] = new long[n];
+        for(int i=0;i<n;i++)
+            temp[i] = arr[(int)arr[i]];
+        for(int i=0;i<n;i++)
+            arr[i] = temp[i];
+    }
+}
