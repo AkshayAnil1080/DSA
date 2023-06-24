be6d1cfc8c1463a4345fcac2ded841e892d0a98d
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-24 14:22:29 +0530

    Added solution - LeetHub

--- Prefix match with other strings - GFG/prefix-match-with-other-strings.java
+++ Prefix match with other strings - GFG/prefix-match-with-other-strings.java
@@ -37,25 +37,69 @@ class GFG
 
 //User function Template for Java
 
+class Trie
+{
+    Trie arr[];
+    int freq;
+    public Trie(int f)
+    {
+        arr=new Trie[26];
+        freq=f;
+    }
+    
+}
+
 
 class Solution
 {
     public int klengthpref(String[] arr, int n, int k, String str)
     {
         // code here
-        int cnt=0;
-        if(k>str.length()) return 0;
+        Trie root = new Trie(0); 
+  
+        for (int i = 0; i < n; i++) 
+        {
+          insert(arr[i], root); 
+        }
         
-       String pre =str.substring(0,k);
-       for(int i=0; i<n; i++)
-       {
-           if(k>arr[i].length()) continue;
-           
-           String curr = arr[i].substring(0,k);
-           if(pre.equals(curr)) // ==
-           cnt++;
-       }
-       return cnt;
-       
+        return find(str,k,root);
     }
+     void insert(String s, Trie root) 
+    { 
+        Trie cur = root; 
+        for (int i = 0; i < s.length(); i++) { 
+            int idx = s.charAt(i) - 'a'; 
+      
+           // if the idx DNE, make a new one else increase the freq.
+            if (cur.arr[idx] == null) 
+                cur.arr[idx] = new Trie(0); 
+      
+            // Increase the frequency of that entry 
+            cur.arr[idx].freq++; 
+      
+            // Move to the next node 
+            cur = cur.arr[idx]; 
+        } 
+    } 
+     int find(String s, int k, Trie root) 
+    { 
+        Trie cur = root; 
+      
+        // Traverse the string 
+        for (int i = 0; i < s.length(); i++) { 
+            int idx = s.charAt(i) - 'a'; 
+      
+            if (cur.arr[idx] == null) 
+                return 0; 
+      
+            // Else traverse to the required node
+            cur = cur.arr[idx]; 
+      
+      
+            // Return the required count 
+            if (i == k-1) 
+                return cur.freq; 
+        } 
+        return 0; 
+    } 
 }
