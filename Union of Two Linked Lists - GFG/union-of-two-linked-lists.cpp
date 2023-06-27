bb112bf9285a733e1c0246f11c96fa163b6445d9
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-06-27 20:41:35 +0530

    Added solution - LeetHub

---
+++ Union of Two Linked Lists - GFG/union-of-two-linked-lists.cpp
@@ -0,0 +1,122 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        set<int> set;
+    
+    while (head1 != nullptr) {
+        set.insert(head1->data);
+        head1 = head1->next;
+    }
+    
+    while (head2 != nullptr) {
+        set.insert(head2->data);
+        head2 = head2->next;
+    }
+    
+    Node* curr = nullptr;
+    Node* start = nullptr;
+    
+    for (int x : set) {
+        if (start == nullptr) {
+            start = new Node(x);
+            curr = start;
+        } else {
+            curr->next = new Node(x);
+            curr = curr->next;
+        }
+    }
+    
+    return start;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
