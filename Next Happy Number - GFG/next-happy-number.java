de2114b73e132479a6bf93c9ef16d9c77c612ee8
Sanskar Gupta <93852920+sanskaarz@users.noreply.github.com>
2023-06-29 10:14:35 +0530

    Corrected Code for POTD 30 June

--- Next Happy Number - GFG/next-happy-number.java
+++ Next Happy Number - GFG/next-happy-number.java
@@ -24,44 +24,44 @@ class GFG
 //User function Template for Java
 
 class Solution { 
-  
-     static int isHappy(int N) { 
-        if (N == 1 || N == 7) 
-            return 1; 
-        int sum = N, curr = N; 
-  
-        // make the sum of square of digits equals a sngle digit num
-        while(sum > 9) { 
-            sum = 0;  // reset sum for every itr
-  
-            // this loop finds the sum of square of digits 
-            while (curr > 0) { 
-                int d = curr%10; // last digit 
-                sum += d*d; 
-                curr/=10; 
-            } 
-            
-            if (sum == 1) 
-                return 1;
-            //else make the new number are curr numnber and proceed ahead.
-            curr = sum; 
+public:
+int nextHappy(int N)
+{   
+    int next=N+1;
+    int res=next;
+    // check for every num
+    while(true)
+    {
+        if(isHappy(next)==1)
+            return next;
+        next++;
+        res=next;
+    }}
+int isHappy(int N) { 
+    if (N == 1 || N == 7) 
+        return 1; 
+    int sum = N, curr = N; 
+
+    // make the sum of square of digits equals a sngle digit num
+    while(sum > 9) { 
+        sum = 0;  // reset sum for every itr
+
+        // this loop finds the sum of square of digits 
+        while (curr > 0) { 
+            int d = curr%10; // last digit 
+            sum += d*d; 
+            curr/=10; 
         } 
-        if(sum == 7) 
-            return 1; 
-        return 0; 
+        
+        if (sum == 1) 
+            return 1;
+        //else make the new number are curr numnber and proceed ahead.
+        curr = sum; 
     } 
-    
-    static int nextHappy(int N)
-    {   
-        int next=N+1;
-        int res=next;
-        // check for every num
-        while(true)
-        {
-            if(isHappy(next)==1)
-                return next;
-            next++;
-            res=next;
-        }
-    }
-}
+    if(sum == 7) 
+        return 1; 
+    return 0; 
+} 
+
+
+};
