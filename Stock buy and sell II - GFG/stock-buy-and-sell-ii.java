cd86001624e79346592c2dc91ce532e397373c38
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-05 15:23:54 +0530

    Added solution - LeetHub

---
+++ Stock buy and sell II - GFG/stock-buy-and-sell-ii.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] prices = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            int res = obj.stockBuyAndSell(n, prices);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int stockBuyAndSell(int n, int[] prices) {
+        // code here
+         int profit=0;
+        for(int i=1; i<n ;i++)
+        {
+            if(prices[i] > prices[i-1])
+            profit+= prices[i] - prices[i-1];
+        }
+        return profit;
+    }
+}
+        
