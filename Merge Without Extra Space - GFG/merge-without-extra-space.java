6362ad96e3391f47d7553829383c2f55353ec3eb
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-07 02:45:50 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.java
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -49,23 +49,38 @@ class Solution
     //Function to merge the arrays.
     public static void merge(long arr1[], long arr2[], int n, int m) 
     {
-          int i = n-1;
-        int j=0;
-        while(i>=0 && j<m)
+         int l = n+m;
+        int gap = (l/2) + (l%2);
+        while(gap>0)
         {
-        if(arr1[i] <=arr2[j])
-        break;
-        
-        else
-        {
-            long temp = arr1[i];
-            arr1[i] =arr2[j];
-            arr2[j] =temp;
-            i--; j++;
+            int st=0; int end=st+gap;
+            while(end<l)
+            {
+                // st in arr1 and end ina arr2
+                if(st<n && end>=n)
+                {
+                    swap(arr1, arr2, st, end-n);
+                }
+                // st in arr2 and end in arr2
+                else if(st>=n)
+                swap(arr2, arr2, st-n, end-n);
+                //st in arr1 and end in arr1
+                else
+                swap(arr1, arr1, st, end);
+                
+                st++; end++;
+            }
+            if(gap==1) break;
+            gap = (gap/2) + (gap%2);
+            
         }
-       
-        }
-         Arrays.sort(arr1);
-        Arrays.sort(arr2);
     }
+    static void swap( long arr1[], long arr2[], int x1, int x2)
+    {
+        if(arr1[x1] > arr2[x2])
+        {
+            long t = arr1[x1]; arr1[x1] = arr2[x2] ; arr2[x2] = t;
+        }
+    }
+    
 }
