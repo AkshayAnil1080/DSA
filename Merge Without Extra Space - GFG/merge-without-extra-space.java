04caba75277c302f95ea408c24ab8a71f9689091
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-07 02:12:24 +0530

    Added solution - LeetHub

---
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    int n = Integer.parseInt(inputLine[0]);
+		    int m = Integer.parseInt(inputLine[1]);
+		    long arr1[] = new long[n];
+		    long arr2[] = new long[m];
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<n; i++){
+		        arr1[i] = Long.parseLong(inputLine[i]);
+		    }
+		    inputLine = br.readLine().trim().split(" ");
+		    for(int i=0; i<m; i++){
+		        arr2[i] = Long.parseLong(inputLine[i]);
+		    }
+		    Solution ob = new Solution();
+		    ob.merge(arr1, arr2, n, m);
+		    
+		    StringBuffer str = new StringBuffer();
+		    for(int i=0; i<n; i++){
+		        str.append(arr1[i]+" ");
+		    }
+		    for(int i=0; i<m; i++){
+		        str.append(arr2[i]+" ");
+		    }
+		    System.out.println(str);
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to merge the arrays.
+    public static void merge(long arr1[], long arr2[], int n, int m) 
+    {
+        long arr3[] = new long[n+m];
+        int k=0;
+        for(long x : arr1)
+        arr3[k++] =x;
+        for(long x : arr2)
+        arr3[k++]=x;
+        
+        Arrays.sort(arr3);
+        for(int i=0; i<n; i++)
+        arr1[i] = arr3[i];
+        
+        for(int i=0; i<m; i++)
+        {
+            arr2[i] = arr3[n+i];
+        }
+    }
+}
