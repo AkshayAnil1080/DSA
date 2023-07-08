dbb14d4b20652b89b5adb57b8d8eef2ebd9e85dc
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-09 02:19:38 +0530

    Added solution - LeetHub

--- Smallest Positive missing number - GFG/smallest-positive-missing-number.java
+++ Smallest Positive missing number - GFG/smallest-positive-missing-number.java
@@ -9,45 +9,19 @@ class Solution
     //Function to find the smallest positive number missing from the array.
     static int missingNumber(int A[], int n)
     {
-       int j = 0;
-        for(int i = 0; i < n; i++)
-        {
-           if (A[i] <= 0)  
-           {
-                int t = A[i];
-               A[i] = A[j];
-               A[j] = t;
-               j++;  
-           }
-        } 
-        // arr2 to store only the +ve integer.
-         int N= n-j;
-        int arr2[] = new int[N];
-        int k=0;
-        for(int i=j;i<n;i++)
-        {
-            arr2[k++] = A[i];
+         for(int i = 0; i < n; ++ i) {
+            while(A[i] >= 1 && A[i] <= n && A[i] != A[A[i]-1])
+                // swap(A[i], A[A[i] - 1]);
+                {
+                    int t = A[A[i] - 1]; A[A[i] - 1] = A[i];  A[i]=t;
+                }
         }
-        // for(int x: arr2)
-        // System.out.println(x+" ");
         
-       // final logic
-       for(int i = 0; i < N; ++ i) {
-      
-        //ignoringm num > N for OOB condition 
-           if(Math.abs(arr2[i]) -1 < N && arr2[Math.abs(arr2[i])-1]>0)
-           {
-               arr2[Math.abs(arr2[i])-1] = -arr2[Math.abs(arr2[i])-1];
-           }
-                
-       }
-       for (int i = 0; i < N; ++i) {
-            if (arr2[i]>0) {
+        for(int i = 0; i < n; ++ i)
+            if(A[i] != i + 1)
                 return i + 1;
-            }
-        }
-
-        return N + 1;
+        
+        return n + 1;
     }
 }
 
