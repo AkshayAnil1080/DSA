3d53f62f26f7aa435439dfc8990c1337f4a60e30
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-10 01:19:35 +0530

    Added solution - LeetHub

---
+++ Transpose of Matrix - GFG/transpose-of-matrix.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine().trim());
+        while(t-->0)
+        {
+            int n=Integer.parseInt(in.readLine().trim());
+            int a[][]=new int[n][n];
+            for(int i=0;i<n;i++){
+                String s[]=in.readLine().trim().split(" ");
+                for(int j=0;j<n;j++){
+                    a[i][j]=Integer.parseInt(s[j]);
+                }
+            }
+            Solution ob=new Solution();
+            ob.transpose(n,a);
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    out.print(a[i][j]+" ");
+                }out.println();
+            }
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public void transpose(int n,int a[][])
+    {
+        for(int i=0; i<n ;i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                int t=a[i][j]; a[i][j]=a[j][i]; a[j][i]=t;
+            }
+        }
+    }
+}
