ad4e549462ac438c32da89686ff1dfccba51bd3a
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-17 03:13:53 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -3,32 +3,69 @@
 using namespace std;
 
 // } Driver Code Ends
+// class Solution {
+// 	public:
+// 		string FirstNonRepeating(string A){
+// 		   string ans = "";
+//         int n = A.length();
+//         int arr[26] = {0};
+//         queue<char> q;
+//         for (int i = 0; i < n; i++)
+//         {
+//             char ch = A[i];
+//             arr[ch - 'a']++;
+//             q.push(ch);
+
+//             while (!q.empty())
+//             {
+//                 if (arr[q.front() - 'a'] == 1)
+//                 {
+//                     ans += q.front();
+//                     break;
+//                 }
+//                 else
+//                     q.pop();
+//             }
+//             if (q.empty())
+//                 ans += '#';
+//         }
+//         return ans;
+// 		}
+
+// };
+
 class Solution {
 	public:
 		string FirstNonRepeating(string A){
-		   string ans = "";
+		    string ans = "";
         int n = A.length();
-        int arr[26] = {0};
-        queue<char> q;
+        
+        vector<vector<int>> freq(26, vector<int>(2)); // store freq and idx
+        
         for (int i = 0; i < n; i++)
         {
             char ch = A[i];
-            arr[ch - 'a']++;
-            q.push(ch);
-
-            while (!q.empty())
-            {
-                if (arr[q.front() - 'a'] == 1)
-                {
-                    ans += q.front();
-                    break;
+            freq[ch - 'a'][0]++; // increase frequency
+            freq[ch - 'a'][1] = i; // store the idx
+            
+            int res_ch = -1;
+            int min_idx = INT_MAX;
+            
+            // Get the character with freq 1 and lowest idx (first non-repeating)
+            for (int j = 0; j < 26; j++) {
+                if (freq[j][0] == 1 && freq[j][1] < min_idx) {
+                    min_idx = freq[j][1];
+                    res_ch = j; // Store its corresponding character
                 }
-                else
-                    q.pop();
             }
-            if (q.empty())
+            
+            if (res_ch == -1) {
                 ans += '#';
+            } else {
+                ans += (char)(res_ch + 'a');
+            }
         }
+        
         return ans;
 		}
 
