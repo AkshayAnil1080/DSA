3215bc5066530644c4dc14e9a2563cdaa58d72c7
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-26 14:50:37 +0530

    Added solution - LeetHub

---
+++ Kth Ancestor in a Tree - GFG/kth-ancestor-in-a-tree.java
@@ -0,0 +1,176 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String arr[] = br.readLine().split(" ");
+	            int k = Integer.parseInt(arr[0]);
+	            int node = Integer.parseInt(arr[1]);
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution g = new Solution();
+        		System.out.println(g.kthAncestor(root,k,node));
+                    t--;
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+Structure of Node class is:
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+
+class Solution
+{
+    int num_node=0;
+    public int kthAncestor(Node root, int k, int node)
+    {
+        //Write your code here
+        //size of tree
+        length(root);
+        //store parent
+        int parent[] = new int[num_node+1];
+        fill_parent(root, parent);
+        
+        //need to move k times
+        int cnt=0;
+        while(node!=-1)
+        {
+            node = parent[node];
+            cnt++;
+            if(cnt==k)
+            return node;
+        }
+        return -1;
+    }
+     void length(Node root)
+    {
+        if(root==null)return;
+        length(root.right);
+        num_node++;
+        length(root.left);
+    }
+    void fill_parent(Node root, int parent[])
+    {
+        parent[root.data] = -1;
+        Queue<Node> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            Node curr = q.poll();
+            if (curr.left!=null)
+            {
+                q.add(curr.left);
+                 parent[curr.left.data] = curr.data;
+            }
+            if (curr.right!=null)
+            {
+                q.add(curr.right);
+                parent[curr.right.data] = curr.data;
+            }
+        } 
+    }
+}
