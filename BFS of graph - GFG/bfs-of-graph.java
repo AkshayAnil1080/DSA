6f1987979d3d04b5ea1be0d59baecdef8c57f6d4
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-07-31 04:14:20 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer>  al =  new ArrayList<>();
+        Queue<Integer>  q =  new LinkedList<>();
+        boolean vis[] = new boolean[V];
+        
+        q.add(0);  // adding the first node
+        
+        vis[0] =  true;  // mark it visited.
+        
+        while(!q.isEmpty())
+        {
+            int curr = q.poll();  // dequeueing and adding into al
+            al.add(curr);
+            
+            for( int neighbour : adj.get(curr) ) 
+            if(!vis[neighbour])
+            {
+                vis[neighbour]  =  true;
+                q.add(neighbour);
+            }
+        }
+        return al;
+    }
+}
