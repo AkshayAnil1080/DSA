f0ca8ae0360abc8a512b32d55c2e8fffcfb1e064
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-08-04 20:36:23 +0530

    Added solution - LeetHub

---
+++ Reverse a Stack - GFG/reverse-a-stack.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        PrintWriter out=new PrintWriter(System.out);
+        
+        //taking testcases
+        int t = Integer.parseInt(br.readLine()); 
+    	for(int i=0;i<t;i++)
+    	{
+            String str=br.readLine();
+    		
+    		//input n and d
+    	    int n=Integer.parseInt(str);
+    		Stack<Integer> stack=new Stack<>();
+    		String str1=br.readLine();
+    		String[] starr1=str1.split(" ");
+    		//inserting elements in the array
+    		for(int j=0;j<n;j++)
+    		{
+    		    stack.push(Integer.parseInt(starr1[j]));
+    		}
+    		//calling reverse() function
+            Solution.reverse(stack);
+            for(int j:stack){
+                out.print(j+" ");
+            }
+            out.println();
+         }
+         out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{ 
+    static void reverse(Stack<Integer> s)
+    {
+        Queue<Integer> q = new LinkedList<>();
+       while(!s.isEmpty())
+       {
+           q.add(s.pop());
+       }
+       while(!q.isEmpty())
+       {
+           s.push(q.remove());
+       }
+    }
+}
