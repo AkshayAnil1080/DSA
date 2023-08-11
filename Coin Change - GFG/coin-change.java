76d9339541f059af5759f38e4db939d01b501cc4
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-08-11 11:47:37 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.java
+++ Coin Change - GFG/coin-change.java
@@ -24,19 +24,56 @@ class GfG {
 
 // User function Template for Java
 
+//rec
+// class Solution {
+//     public long count(int coins[], int n, int sum) {
+//         // code here.
+//         return solve(coins, n, sum);
+//     }
+//   long solve(int coins[], int n, int sum)
+//   {
+//       if(sum<0) return 0;
+//       if(n<=0) return 0;
+       
+//       if(sum==0) return 1;
+       
+//       return 
+//       solve(coins, n-1,sum) + solve(coins, n, sum-coins[n-1]);
+//   }
+// }
+
+// memo
+// class Solution {
+//     public long count(int coins[], int n, int sum) {
+//         // code here.
+//         long dp[] = new long[sum+1];
+//         dp[0] =1;
+        
+//         for(int i =0; i<n; i++){
+//           for(int j = coins[i] ; j<=sum ; j++)
+//           {
+//               dp[j] = dp[j] + dp[j-coins[i]];
+//           }
+//         }
+       
+//     return dp[sum];        
+//     }
+// }
+//tab
 class Solution {
     public long count(int coins[], int n, int sum) {
         // code here.
+        
         long dp[] = new long[sum+1];
         dp[0] =1;
         
-        for(int i =0; i<n; i++){
-          for(int j = coins[i] ; j<=sum ; j++)
-          {
-              dp[j] = dp[j] + dp[j-coins[i]];
-          }
+        for(int i=0; i<n ;i++ ) //for every coins
+        {
+            for(int j=coins[i] ;j<=sum; j++){ // itr in sum
+                dp[j] = dp[j] + dp[j-coins[i]];
+            }
         }
-       
-    return dp[sum];        
+        
+        return dp[sum];
     }
 }
