44390d4988301927552657448d6fcfe886317df3
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-08-20 01:59:56 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.java
+++ Number of occurrence - GFG/number-of-occurrence.java
@@ -48,21 +48,83 @@ public class Main {
 
 
 
+// class Solution {
+//     int count(int[] arr, int n, int x) {
+//         // code here
+//         HashMap<Integer, Integer>  mp = new HashMap<>();
+//         int cnt=0;
+        
+//         for(int i=0; i<n; i++)
+//         mp.put(arr[i], mp.getOrDefault(arr[i],0)+1);
+        
+//         for(Map.Entry<Integer, Integer> z : mp.entrySet())
+//         {
+//             if(z.getKey()==x)
+//             cnt = z.getValue();
+//         }
+        
+//         return cnt;
+//     }
+// }
+
+
 class Solution {
     int count(int[] arr, int n, int x) {
         // code here
-        HashMap<Integer, Integer>  mp = new HashMap<>();
-        int cnt=0;
+        int  first = l_idx(arr, x);
+        int last = h_idx(arr,x);
         
-        for(int i=0; i<n; i++)
-        mp.put(arr[i], mp.getOrDefault(arr[i],0)+1);
+        // System.out.println(first +  " " + last);
+        if(last==-1)
+        return 0;
         
-        for(Map.Entry<Integer, Integer> z : mp.entrySet())
+        return last-first+1;
+    }
+    int h_idx(int arr[], int x)
+    {
+        int n = arr.length;
+        int idx=-1;
+        int low=0; int high=n-1;
+        while(low<=high)
         {
-            if(z.getKey()==x)
-            cnt = z.getValue();
+            int mid = (low+high)/2;
+            if(arr[mid] > x)
+            high = mid-1;
+            
+            else if(arr[mid]==x)
+            {
+                idx=mid; low=mid+1;
+            }
+            
+            // if(arr[mid] < x)
+            else
+            low= mid+1;
+            
         }
-        
-        return cnt;
+        return idx;
+    }
+    
+    int l_idx(int arr[], int x)
+    {
+        int n = arr.length;
+        int idx=-1;
+        int low=0; int high=n-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid] > x)
+            high = mid-1;
+            
+            else if(arr[mid]==x)
+            {
+             idx=mid; high=mid-1;
+            }
+            
+            // if(arr[mid] < x)
+            else
+            low= mid+1;
+            
+        }
+         return idx;
     }
 }
