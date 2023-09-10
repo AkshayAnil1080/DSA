b085538db8450ab538c3faba46550f5cac5da413
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-11 01:41:42 +0530

    Added solution - LeetHub

--- Lucky Numbers - GFG/lucky-numbers.java
+++ Lucky Numbers - GFG/lucky-numbers.java
@@ -36,6 +36,7 @@ class Solution
     // Return True if the given number is a lucky number else return False
     public static boolean isLucky(int n)
     {
+        // Your code here
         for(int i=2; i<=n; i++)
         {
             if(n%i==0)
