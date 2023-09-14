aa22c1c1e1128999de3c5e2f7829b0dbb9fb5bb6
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-15 01:45:25 +0530

    Added solution - LeetHub

---
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            int N = Integer.parseInt(in.readLine());
+            String input_line[] = in.readLine().trim().split("\\s+");
+            int arr[] = new int[N];
+            for(int i = 0;i < N;i++)
+                arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            int x = ob.equalPartition(N, arr);
+            if(x == 1)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+// class Solution{
+//     static int equalPartition(int N, int arr[])
+//     {
+//         // code here
+//           int totalSum = 0;
+//         for (int i=0;i<N;i++)
+//             totalSum += arr[i];
+    
+//         if (totalSum % 2 != 0)
+//             return 0;
+    
+//         int targetSum = totalSum / 2;
+        
+//          return fun(0, targetSum, arr)==true ? 1:0;
+//     }
+//      static boolean fun(int pos, int sum, int arr[])
+//     {
+//         if(sum<0) return false;
+//         if (pos == arr.length)
+//             return sum==0? true:false; 
+            
+//         return fun(pos + 1, sum,arr)  ||  fun(pos + 1, sum - arr[pos], arr); //not take + take
+//     }
+// }
+
+class Solution{
+    static int equalPartition(int N, int arr[])
+    {
+        // code here
+          int totalSum = 0;
+        for (int i = 0; i < N; i++)
+            totalSum += arr[i];
+
+        if (totalSum % 2 != 0)
+            return 0;
+
+        int targetSum = totalSum / 2;
+
+        boolean[][] memo = new boolean[N + 1][targetSum + 1];// all marked as false now
+        return fun(0, targetSum, arr, memo) ? 1 : 0;
+    }
+
+    static boolean fun(int pos, int sum, int arr[], boolean[][] memo) {
+        if (sum < 0)
+            return false;
+        if (pos == arr.length)
+            return sum == 0;
+
+        if (memo[pos][sum])
+            return true;
+
+        return memo[pos][sum] = fun(pos + 1, sum - arr[pos], arr, memo) ||  fun(pos + 1, sum, arr, memo);
+    }
+}
+
