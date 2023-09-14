0bf48088b4bfe23b99b95768d909cf992e1bcf6a
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-15 01:56:28 +0530

    Update partition-equal-subset-sum.cpp

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -1,3 +1,5 @@
+
+
 //{ Driver Code Starts
 // Initial Template for C++
 
@@ -7,6 +9,71 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+//rec
+class Solution{
+public:
+    int equalPartition(int N, int arr[])
+    {
+        int totalSum = 0;
+    for (int i = 0; i < N; i++)
+        totalSum += arr[i];
+
+    if (totalSum % 2 != 0)
+        return 0;
+
+    int targetSum = totalSum / 2;
+
+    return fun(0,N, targetSum, arr) ? 1 : 0;
+}
+
+bool fun(int pos, int n, int sum, int arr[]) {
+    if (sum < 0)
+        return false;
+    if (pos == n)
+        return sum == 0;
+
+    return fun(pos + 1, n, sum - arr[pos], arr) || fun(pos + 1, n, sum, arr);
+}
+};
+
+
+
+//memo
+class Solution{
+public:
+bool fun(int pos, int n, int sum, int arr[], vector<vector<bool>>& memo) {
+    if (sum < 0)
+        return false;
+    if (pos == n)
+        return sum == 0;
+
+    if (memo[pos][sum])
+        return true;
+
+    return memo[pos][sum] = fun(pos + 1, n, sum - arr[pos], arr, memo) || fun(pos + 1, n, sum, arr, memo);
+}
+
+    int equalPartition(int N, int arr[])
+    {
+      int totalSum = 0;
+    for (int i = 0; i < N; i++)
+        totalSum += arr[i];
+
+    if (totalSum % 2 != 0)
+        return 0;
+
+    int targetSum = totalSum / 2;
+
+    vector<vector<bool>> memo(N + 1, vector<bool>(targetSum + 1, false));
+
+    return fun(0, N, targetSum, arr, memo) ? 1 : 0;
+}
+
+
+};
+
+
+//tab
 class Solution{
 public:
     int equalPartition(int n, int arr[])
