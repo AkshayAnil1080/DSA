914658b554dc10d61e02ff70dc9549a5e942c25a
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-19 01:44:35 +0530

    Added solution - LeetHub

--- Find first set bit - GFG/find-first-set-bit.java
+++ Find first set bit - GFG/find-first-set-bit.java
@@ -23,26 +23,40 @@ class Main {
 
 
 //User function Template for Java
-
+//LogN
+// class Solution
+// {
+//     //Function to find position of first set bit in the given number.
+//     public static int getFirstSetBit(int n){
+            
+//       if(n==0)
+//             return 0;
+            
+//         int cnt  = 0;
+//         while(n > 0)
+//         {
+//             cnt++;
+//             if((n&1)==1)
+//             return cnt;
+            
+//             n = n /2;   // n>>1;
+//         }
+        
+//       return 0;
+            
+//     }
+// }
+//logN
 class Solution
 {
     //Function to find position of first set bit in the given number.
     public static int getFirstSetBit(int n){
             
-       if(n==0)
+            if(n==0)
             return 0;
             
-        int cnt  = 0;
-        while(n > 0)
-        {
-            cnt++;
-            if((n&1)==1)
-            return cnt;
-            
-            n = n /2;   // n>>1;
-        }
-        
-      return 0;
+      return (int) (Math.log(n&-n)/Math.log(2) ) +1;
+       
             
     }
 }
