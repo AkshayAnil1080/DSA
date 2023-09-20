85469de86f2f181620607bb461432094bbc25ba7
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-21 01:49:28 +0530

    Added solution - LeetHub

--- Stickler Thief - GFG/stickler-thief.java
+++ Stickler Thief - GFG/stickler-thief.java
@@ -30,27 +30,65 @@ class GFG
 }
 // } Driver Code Ends
 
+//rec O(2^n), O(n)
+// class Solution
+// {
+//     //Function to find the maximum money the thief can get.
+//     public int FindMaxSum(int arr[], int n)
+//     {
+//          return helper(arr, 0);
+//     }
+//     public int helper(int [] nums, int index)
+//     {
+//         if(index  >=nums.length) return 0;
+//         return Math.max(helper(nums, index + 2) + nums[index], helper(nums, index +1));
+//     }
+// }
+
+//memo O(n), O(n)
+// class Solution
+// {
+//     //Function to find the maximum money the thief can get.
+//     public int FindMaxSum(int arr[], int n)
+//     {
+//          int[] memo = new int[n];
+//         Arrays.fill(memo, -1); 
+//         // Initialize the memoization array with -1 to indicate uncalculated values
+//         return helper(arr, 0, memo);
+//     }
+
+//     public int helper(int[] nums, int index, int[] memo) {
+//         if (index >= nums.length)
+//             return 0;
+
+//         // Check if the result for the current index is already calculated
+//         if (memo[index] != -1)
+//             return memo[index];
+
+//         // Calculate the result and store it in memo array
+//       return  memo[index] = Math.max(helper(nums, index + 2, memo) 
+//       + nums[index], helper(nums, index + 1, memo));
+//     }
+// }
+// tab O(n), O(N)
 class Solution
 {
     //Function to find the maximum money the thief can get.
     public int FindMaxSum(int arr[], int n)
     {
-         int[] memo = new int[n];
-        Arrays.fill(memo, -1); 
-        // Initialize the memoization array with -1 to indicate uncalculated values
-        return helper(arr, 0, memo);
-    }
-
-    public int helper(int[] nums, int index, int[] memo) {
-        if (index >= nums.length)
+      if (n == 0) {
             return 0;
+        }
 
-        // Check if the result for the current index is already calculated
-        if (memo[index] != -1)
-            return memo[index];
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = arr[0];
 
-        // Calculate the result and store it in memo array
-       return  memo[index] = Math.max(helper(nums, index + 2, memo) 
-       + nums[index], helper(nums, index + 1, memo));
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2] + arr[i - 1], dp[i - 1]);
+        }
+
+        return dp[n];
     }
 }
+
