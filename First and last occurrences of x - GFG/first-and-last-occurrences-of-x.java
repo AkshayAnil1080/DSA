c72467664d9050f8fa79ba20cacd8a4ab16d37c8
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-22 04:25:56 +0530

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class GFG
+{
+    ArrayList<Integer> find(int arr[], int n, int x)
+    {
+        // code here
+        int first=-1, last=-1;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i]==x)
+            {
+                if(first==-1)
+                first=i;
+                
+                last=i;
+            }
+        }
+        ArrayList<Integer> al = new ArrayList<Integer>();
+        al.add(first); al.add(last);
+        
+        return al; 
+    }
+}
+
+
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int x =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(a1[i]);
+            }
+            GFG ob = new GFG();
+            ArrayList<Integer> ans=ob.find(arr,n,x);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
