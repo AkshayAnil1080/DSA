90d21abdd68f1f16dee5fced89acd7631f65b6ef
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-22 04:42:09 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.cpp
@@ -4,29 +4,90 @@ using namespace std;
 
 // } Driver Code Ends
 
+// class Solution
+// {
+//     public:
+//     vector<int> find(int arr[], int n , int x )
+//     {
+//          int first=-1, last=-1;
+//         for(int i=0; i<n; i++)
+//         {
+//             if(arr[i]==x)
+//             {
+//                 if(first==-1)
+//                 first=i;
+                
+//                 last=i;
+//             }
+//         }
+//         vector<int> al;
+//         al.push_back(first); al.push_back(last);
+        
+//         return al; 
+//     }
+// };
+
+
 class Solution
 {
     public:
     vector<int> find(int arr[], int n , int x )
     {
          int first=-1, last=-1;
-        for(int i=0; i<n; i++)
-        {
-            if(arr[i]==x)
-            {
-                if(first==-1)
-                first=i;
-                
-                last=i;
-            }
-        }
+         first = l_idx(arr, x,n);
+         last = h_idx(arr,x,n);
         vector<int> al;
         al.push_back(first); al.push_back(last);
+  
+        return al;
         
-        return al; 
+    }
+    int h_idx(int arr[], int x, int n)
+    {
+        int idx=-1;
+        int low=0; int high=n-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid] > x)
+            high = mid-1;
+            
+            else if(arr[mid]==x)
+            {
+                idx=mid; low=mid+1;
+            }
+            
+            // if(arr[mid] < x)
+            else
+            low= mid+1;
+            
+        }
+        return idx;
+    }
+    
+    int l_idx(int arr[], int x, int n)
+    {
+        int idx=-1;
+        int low=0; int high=n-1;
+        while(low<=high)
+        {
+            int mid = (low+high)/2;
+            if(arr[mid] > x)
+            high = mid-1;
+            
+            else if(arr[mid]==x)
+            {
+             idx=mid; high=mid-1;
+            }
+            
+            // if(arr[mid] < x)
+            else
+            low= mid+1;
+            
+        }
+         return idx;
     }
 };
-
 
 //{ Driver Code Starts.
 
