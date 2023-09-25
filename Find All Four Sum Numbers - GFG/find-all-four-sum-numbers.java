c1a6201459771119724d763497f86407d48e1211
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-26 03:25:25 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.java
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.java
@@ -44,27 +44,63 @@ class GFG {
 // arr[] : int input array of integers
 // k : the quadruple sum required
 
+// class Solution {
+//     public ArrayList<ArrayList<Integer>> fourSum(int[] arr, int S) {
+//         // code here
+//          int n = arr.length;
+//          ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
+//         HashSet<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>(); // Step 1
+//         Arrays.sort(arr);
+        
+//         for (int i = 0; i < n-3; i++) {
+            
+//             for (int j = i + 1; j < n-2; j++) {
+//                 for (int k = j + 1; k < n-1; k++) {
+//                     for (int l = k + 1; l < n; l++) {
+//                         int sum = arr[i] + arr[j] + arr[k] + arr[l];
+
+//                         if (sum == S) {
+//                             ArrayList<Integer> temp = new  ArrayList<Integer>(
+//                             Arrays.asList(arr[i], arr[j], arr[k], arr[l]));
+//                             if(!set.contains(temp)){
+//                             set.add(temp); // Step 2
+//                             ans.add(temp);
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+    
+//         return ans;
+//     }
+// }
+
 class Solution {
     public ArrayList<ArrayList<Integer>> fourSum(int[] arr, int S) {
         // code here
          int n = arr.length;
          ArrayList<ArrayList<Integer>> ans = new ArrayList<ArrayList<Integer>>();
-        HashSet<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>(); // Step 1
+        // HashSet<ArrayList<Integer>> set = new HashSet<ArrayList<Integer>>(); // Step 1
         Arrays.sort(arr);
         
         for (int i = 0; i < n-3; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
             for (int j = i + 1; j < n-2; j++) {
+                 if (j > i + 1 && arr[j] == arr[j - 1]) continue;
                 for (int k = j + 1; k < n-1; k++) {
+                     if (k > j + 1 && arr[k] == arr[k - 1]) continue;
                     for (int l = k + 1; l < n; l++) {
+                         if (l > k + 1 && arr[l] == arr[l - 1]) continue;
                         int sum = arr[i] + arr[j] + arr[k] + arr[l];
 
                         if (sum == S) {
                             ArrayList<Integer> temp = new  ArrayList<Integer>(
                             Arrays.asList(arr[i], arr[j], arr[k], arr[l]));
-                            if(!set.contains(temp)){
-                            set.add(temp); // Step 2
+                            // if(!set.contains(temp)){
+                            // set.add(temp); // Step 2
                             ans.add(temp);
-                            }
+                            // }
                         }
                     }
                 }
