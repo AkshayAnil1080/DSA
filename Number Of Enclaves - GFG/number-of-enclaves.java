fc5ab65f26917673d7d0eb908a1481836220ac4c
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-09-29 01:29:45 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.java
+++ Number Of Enclaves - GFG/number-of-enclaves.java
@@ -37,60 +37,94 @@ class GFG {
 // User function Template for Java
 
 
+// class Solution {
+
+//     int numberOfEnclaves(int[][] a) {
+
+//       int n = a.length; int m= a[0].length;
+//         	for(int i = 0;i < n;i++){
+// 			for(int j = 0;j < m;j++){
+// 			    if(i==0 || j==0 || i==n-1 || j==m-1)
+// 			    dfs(a, i,j,n,m);
+// 			}
+//     	}
+	
+			    
+// 			 int cnt =0;
+// 		for(int i = 0;i < n;i++)
+// 			for(int j = 0;j < m;j++)
+// 				cnt+=a[i][j];
+				    
+
+// 		return cnt;
+
+//     }
+//      static void dfs(int a[][], int x, int y, 
+//                         int N, int M)
+//     {
+//         // out of bound
+//         if (x < 0 || x >= N || y < 0 || y >= M)
+//             return;
+//         if(a[x][y]==0)
+//         return;
+//         // mark with newC and itr in 4 neighbours
+//         a[x][y] = 0;
+//         dfs(a, x+1, y,  N, M);
+//         dfs(a, x-1, y,  N, M);
+//         dfs(a, x, y+1,  N, M);
+//         dfs(a, x, y-1,  N, M);
+//     }
+// }
+
 class Solution {
 
     int numberOfEnclaves(int[][] a) {
+        int n = a.length;
+        int m = a[0].length;
 
-       int n = a.length; int m= a[0].length;
-        for(int i = 0;i < n;i++){
-		    if(a[i][0] == 1) //1st col
-			    dfs(a, i, 0,  n, m);
-			    
-			 if(a[i][m-1] == 1) //last col
-			    dfs(a, i, m-1, n, m);
-		}
+        // Create a queue for BFS
+        Queue<int[]> queue = new LinkedList<>();
 
-			    
-		
-		for(int i = 0;i < m;i++){
-			if(a[0][i] == 1) //1st row
-			    dfs(a, 0, i,  n, m);
-			 if(a[n-1][i] == 1)  	// last row
-			    dfs(a, n-1, i,  n, m);
-		}
-		
-		
-// 			for(int i = 0;i < n;i++){
-// 			for(int j = 0;j < m;j++){
-// 		            System.out.print(a[i][j]+" ");
-// 		        }
-// 		        System.out.println();
-//         }
-		        
-			    
-			 int cnt =0;
-		for(int i = 0;i < n;i++)
-			for(int j = 0;j < m;j++)
-				cnt+=a[i][j];
-				    
+        // Add the boundary cells to the queue
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 || j == 0 || i == n - 1 || j == m - 1) && a[i][j] == 1) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
 
-		return cnt;
+        // Perform BFS
+        while (!queue.isEmpty()) {
+            int[] cell = queue.poll();
+            int x = cell[0];
+            int y = cell[1];
+            
+            // Mark the cell as visited
+            a[x][y] = 0;
 
-    }
-     static void dfs(int a[][], int x, int y, 
-                        int N, int M)
-    {
-        // out of bound
-        if (x < 0 || x >= N || y < 0 || y >= M)
-            return;
-        if(a[x][y]==0)
-        return;
-        // mark with newC and itr in 4 neighbours
-        a[x][y] = 0;
-        dfs(a, x+1, y,  N, M);
-        dfs(a, x-1, y,  N, M);
-        dfs(a, x, y+1,  N, M);
-        dfs(a, x, y-1,  N, M);
+            // Explore the neighboring cells
+            int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+            for (int[] dir : directions) {
+                int newX = x + dir[0];
+                int newY = y + dir[1];
+                
+                // Check if the neighbor is within bounds and unvisited
+                if (newX >= 0 && newX < n && newY >= 0 && newY < m && a[newX][newY] == 1) {
+                    queue.offer(new int[]{newX, newY});
+                }
+            }
+        }
+
+        // Count the remaining '1's in the grid
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                cnt += a[i][j];
+            }
+        }
+
+        return cnt;
     }
 }
     
