8ee2339d3a97a66e241811bfb7fc52938e2253e6
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2023-10-11 01:00:33 +0530

    Added solution - LeetHub

---
+++ Check for Balanced Tree - GFG/check-for-balanced-tree.java
@@ -0,0 +1,163 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+			
+			    if(g.isBalanced(root) == true)
+    			    System.out.println(1);
+    			else 
+    			    System.out.println(0);
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+/* A binary tree node class
+class Node
+{
+	int data;
+	Node left,right;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;		
+	}
+} */
+
+class Tree
+{
+    
+    //Function to check whether a binary tree is balanced or not.
+    boolean isBalanced(Node root)
+    {
+	return checkBalance(root) != -1;
+    }
+
+    private int checkBalance(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lht = checkBalance(root.left);
+        if (lht == -1) {
+            return -1; // Left subtree is unbalanced, so the whole tree is unbalanced.
+        }
+
+        int rht = checkBalance(root.right);
+        if (rht == -1) {
+            return -1; // Right subtree is unbalanced, so the whole tree is unbalanced.
+        }
+
+        int heightDiff = Math.abs(lht - rht);
+        if (heightDiff > 1) {
+            return -1; // marking the tree is unbalanced.
+        }
+
+        return 1 + Math.max(lht, rht); // Return the height of the current subtree.
+    }
+}
+
+
