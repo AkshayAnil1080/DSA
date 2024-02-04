d05b244ce9affb8d4b141dca515ac93747942cf0
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2024-02-04 20:20:06 +0530

    Update 5. Longest Palindromic Substring -LeetCode.java

--- 5. Longest Palindromic Substring /5. Longest Palindromic Substring -LeetCode.java
+++ 5. Longest Palindromic Substring /5. Longest Palindromic Substring -LeetCode.java
@@ -1,92 +1,52 @@
 class Solution {
-    public int countSubstrings(String s) {
-        int n = s.length(), ans = 0;
-
-        for(int i=0; i<n ; i++){
-            StringBuilder str = new StringBuilder();
-            for(int j =i; j<n; j++){
-                str.append(s.charAt(j));
-                if(isPalin(str.toString()))
-                ans++;
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int start=0; int end=0;
+        boolean dp[][]=  new boolean[n][n];
+        for(int i=n-1; i>=0 ;i--){
+            for(int j=i+1; j<n; j++){
+                dp[i][j] = s.charAt(i)==s.charAt(j) && (j-i<=2 || dp[i+1][j-1]);
+                if(dp[i][j] && j-i > end-start){
+                    start=i; end=j;
+                }
             }
         }
-        return ans;
-    }
-    boolean isPalin(String str){
-        int st=0; int end = str.length()-1;
-        while(st<=end){
-            if(str.charAt(st)!=str.charAt(end))
-            return false;
-            st++; end--;
-        }
-        return true;
-    }
-
-}
+        return s.substring(start, end+1);|
 
 class Solution {
-    public int countSubstrings(String s) {
-        int n = s.length(), ans = 0;
-
-        if (n <= 0) 
-            return 0;
-
-        boolean[][] dp = new boolean[n][n];
-
-        // Base case: single letter substrings
-        for (int i = 0; i < n; i++){
-            dp[i][i] = true;
-            ans++;
-        } 
-            
-    
-        // Base case: double letter substrings
-        for (int i = 0; i < n - 1; ++i) {
-            dp[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
-            ans += (dp[i][i + 1] ? 1 : 0);
-        }
-
-        // All other cases: substrings of length 3 to n
-        for (int len = 3; len <= n; ++len)
-            for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
-                dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
-                ans += (dp[i][j] ? 1 : 0);
-            }
-
-        return ans;
-    }
-}
-
-class Solution {
-     int ans;
-    public int countSubstrings(String s) {
-          ans=s.length();
+    int low;
+    int maxLength;
+    public String longestPalindrome(String s) {
+      low=0; maxLength=0;
         int n = s.length();
         int st=0, end=0;
-        for(int i=1; i<n; i++)
+        for(int i=0; i<n; i++)
         {
-            //1 even  
-            st=i-1; end=i;
+            //1 odd
+            st=i; end=i;
             helper(s, st, end);
-            //odd
-            st = i-1; end=i+1;
+            //even
+            st = i; end=i+1;
             helper(s, st, end);
             
             
         }
-        return ans;
+         return s.substring(low, low + maxLength);
+         
     }
-     int helper(String str, int i, int j){
+     void helper(String str, int i, int j){
         while(i>=0 && j<str.length()){
             if(str.charAt(i)!=str.charAt(j))
-            return ans;
+            return;
             
             else{
-                ans++;
+                 int length = j-i+1;
+                if(length > maxLength){
+                    low=i;
+                    maxLength=length;
+                }
                 i--; j++;
             }
-        }
-        return ans;
+        } 
     }
-    
 }
