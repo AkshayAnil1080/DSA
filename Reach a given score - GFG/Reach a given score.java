c45dda7c61fdb904e0c1c370a953c2b9b71b4300
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2024-02-25 16:46:10 +0530

    Update Reach a given score.java

--- Reach a given score - GFG/Reach a given score.java
+++ Reach a given score - GFG/Reach a given score.java
@@ -14,3 +14,30 @@ class Geeks {
         return dp[n];
     }
 }
+
+
+
+
+class Geeks {
+    ArrayList<Long> numbers = new ArrayList<>();
+
+    public long count(int n) {
+        numbers.add(3L);
+        numbers.add(5L);
+        numbers.add(10L);
+
+        return solve(n, 0);
+    }
+
+    private long solve(int remaining, int index) {
+        if (remaining == 0) {
+            return 1;
+        }
+
+        if (index >= 3 || remaining < 0) {
+            return 0;
+        }
+
+        return solve(remaining - numbers.get(index).intValue(), index) + solve(remaining, index + 1);
+    }
+}
