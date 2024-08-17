d9fb5a778e079355a0e4e014a7cd63940fa7976b
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2024-08-17 16:18:47 +0530

    Create Product array puzzle.java

---
+++ Product array puzzle -GFG/Product array puzzle.java
@@ -0,0 +1,37 @@
+
+class Solution {
+    public static long[] productExceptSelf(int nums[]) {
+        // code here
+        int cnt_zero=0;
+        long prod=1;
+        for(int x : nums){
+            if(x!=0)
+            prod *=x;
+            else 
+            cnt_zero++;
+        }
+        // System.out.println(prod);
+        long arr[] = new long[nums.length];
+        if(cnt_zero>1){
+            return arr;
+        }
+        else{
+            for(int i=0; i<nums.length; i++){
+                if(nums[i]!=0){
+                    if(cnt_zero==1)
+                    arr[i]=0;
+                    // else
+                    // arr[i] = prod/nums[i];
+                    else{
+                        arr[i] = prod/nums[i]; 
+                    }
+                }
+               
+                else
+                arr[i] = prod;
+            }
+        }
+        return arr;
+        
+    }
+}
