09b239129eb0cda8d783a2272443d6cc3f2ec5ce
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2025-07-09 14:38:27 +0530

    Time: 4 ms (77.92%), Space: 44.2 MB (90.54%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -8,46 +8,35 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-//  time : O(k * k * n)
-// space : O(1)
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-       ListNode res = null;
-       for(ListNode curr : lists){
-        res = merge(res, curr);
-       }
-       return res;
-    }
-    //   TC: O(n+m) ~ k*n =>  n = number of ele in each list. 
-    // n+ 2n+ 3n +....n*k
- //SC: O(1)
-    ListNode merge(ListNode h1, ListNode h2){
-       // 1. Create a dummy node to track the head of the resultant list
-        //1. crete dummy node to track head of the resultant LL
+         // Step 1: Define a min heap based on ListNode value
+        PriorityQueue<ListNode> minHeap = new PriorityQueue<>(
+            (a, b) -> a.val - b.val
+        );
+
+
+        // Step 2: Add head of each non-null list to the minHeap
+        for (ListNode node : lists) {
+            if (node != null) {
+                minHeap.add(node);
+            }
+        }
+
+        // Step 3: Create a dummy node to build the result
         ListNode dummy = new ListNode(-1);
-        ListNode curr = dummy; 
+        ListNode curr = dummy;
 
-        //2. traverse from both ll and (add, iterate) in the smaller one 
-        while(h1!=null && h2!=null){
-            if(h1.val <= h2.val){
-            curr.next=h1;
-              h1= h1.next;
+        // Step 4: Build the merged list
+        while (!minHeap.isEmpty()) {
+            ListNode smallest = minHeap.poll(); // getting node smallest value
+            curr.next = smallest;
+            curr = curr.next;
+
+            if (smallest.next != null) {
+                minHeap.add(smallest.next); // push next node from same list
             }
-            else{
-                curr.next = h2;
-                h2=h2.next;
-            }
-
-             curr=curr.next;
         }
-        //traverse through either remaining non empty list
-        if(h1!=null){
-            curr.next = h1;
-        }
-        else{
-            curr.next=h2;
-        }
-
         return dummy.next;
     }
 }
