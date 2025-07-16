e062b0c7f96069fbe15f46ae41ca2b005e3860a6
AKSHAY ANIL <46106691+AkshayAnil1080@users.noreply.github.com>
2025-07-16 18:44:41 +0530

    Time: 15 ms (28.39%), Space: 48.3 MB (28.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,15 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] arr) {
             // code here
-        int n = arr.length;
+      // code here
+         int n = arr.length;
         List<List<String>> al = new ArrayList<>();
         HashMap<String, Integer> map = new HashMap<>();
         
         for(int i=0; i<n ;i ++){
-            //sort the stirngs 
-            char temp[] = arr[i].toCharArray();
-            Arrays.sort(temp);
-            String curr = new String(temp);
+            String curr = getHash(arr[i]);
             
-            //if not in map - new anagram group found, initilize al, and add into map
+        //  if not in map - new anagram group found, initilize al, and add into map
             if(!map.containsKey(curr)){
                 map.put(curr, al.size()); // we need to group the angrams in 2d al
                 al.add(new ArrayList<>());
@@ -19,6 +17,19 @@ class Solution {
             // else - anagrams found, place them into respective grps
             al.get(map.get(curr)).add(arr[i]);
         }
-        return al;
+        return al; 
+    }
+
+    String getHash(String str){
+        int freq[] = new int [26];
+        for(char ch : str.toCharArray()){
+            freq[ch-'a']++;
+        }
+        StringBuilder hash = new StringBuilder();
+        for(int x: freq){
+            hash.append(x);
+            hash.append('#'); // delimiter,[ convetion]. however, in this case not req.
+        }
+        return hash.toString();
     }
 }
